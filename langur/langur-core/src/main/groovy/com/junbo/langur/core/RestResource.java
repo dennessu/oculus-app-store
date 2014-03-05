d6fb9499d201b8fca9a01a01a0b6ce636ffc0d4f
kg <kg@wan-san.com>
2014-03-06 00:14:24 +0800

    remove unnecessary wrap

--- langur/langur-core/src/main/groovy/com/junbo/langur/core/RestResource.java
+++ langur/langur-core/src/main/groovy/com/junbo/langur/core/RestResource.java
@@ -12,7 +12,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Java doc.
+ * RestResource.
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
