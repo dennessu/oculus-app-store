20666dca7d8ec64d8ebad4d21ab4945883ea59fe
czjy <cz@wan-san.com>
2014-03-07 14:52:10 +0800

    fix ut

--- order/order-core/src/test/groovy/com/junbo/order/core/orderflow/FlowSelectorTest.groovy
+++ order/order-core/src/test/groovy/com/junbo/order/core/orderflow/FlowSelectorTest.groovy
@@ -1,5 +1,4 @@
 package com.junbo.order.core.orderflow
-
 import com.junbo.order.core.BaseTest
 import com.junbo.order.core.FlowType
 import com.junbo.order.core.OrderServiceOperation
@@ -7,17 +6,22 @@ import com.junbo.order.core.common.TestBuilder
 import com.junbo.order.core.impl.orderflow.DefaultFlowSelector
 import groovy.transform.CompileStatic
 import org.testng.annotations.Test
+
+import javax.annotation.Resource
 /**
  * Created by chriszhu on 2/7/14.
  */
 @CompileStatic
 class FlowSelectorTest extends BaseTest {
 
+    @Resource(name = 'defaultFlowSelector')
+    DefaultFlowSelector flowSelector
+
     @Test
     void testSelector_CREATE_PayIn_CreditCard_Digital() {
         def context = TestBuilder.buildDefaultContext()
         context.paymentInstruments = [TestBuilder.buildCreditCartPI()]
-        new DefaultFlowSelector().select(context, OrderServiceOperation.CREATE).syncThen { FlowType type ->
+        flowSelector.select(context, OrderServiceOperation.CREATE).syncThen { FlowType type ->
             assert(type == FlowType.IMMEDIATE_SETTLE)
         }
     }
@@ -26,7 +30,7 @@ class FlowSelectorTest extends BaseTest {
     void testSelector_CREATE_PayIn_NoPI_Digital() {
         def context = TestBuilder.buildDefaultContext()
         context.order.paymentInstruments = null
-        new DefaultFlowSelector().select(context, OrderServiceOperation.CREATE).syncThen { FlowType type ->
+        flowSelector.select(context, OrderServiceOperation.CREATE).syncThen { FlowType type ->
             assert(type == FlowType.FREE_SETTLE)
         }
     }
