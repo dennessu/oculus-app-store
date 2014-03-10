f8d607729006744038b9fbada033ba4e71e87e5b
zhany <zhany@wan-san.com>
2014-03-11 01:08:14 +0800

    add end-session endpoint

--- oauth/oauth-app/src/main/groovy/com/junbo/oauth/app/Main.groovy
+++ oauth/oauth-app/src/main/groovy/com/junbo/oauth/app/Main.groovy
@@ -35,7 +35,7 @@ class Main {
         resourceConfig.property('contextConfigLocation', 'classpath*:/spring/**/*.xml')
         resourceConfig.register(RestExceptionMapper)
 
-        def uri = URI.create('http://0.0.0.0:8082/rest')
+        def uri = URI.create('http://0.0.0.0:8082/auth')
         return GrizzlyHttpServerFactory.createHttpServer(uri, resourceConfig)
     }
 
