a819c8c210a85d93ead55e000635b7a09dd077ea
Shu Zhang <shuz@wan-san.com>
2014-03-11 18:06:59 +0800

    Change the build script to refer to dirs.
    The build script is also updated in both mac and windows. The dirs to
    build will be in sync in both system.

--- build.bat
+++ build.bat
@@ -3,152 +3,21 @@ set gradle_arg=clean build install %1
 for %%i in (%*) do (
   set gradle_arg=%gradle_arg% %%i
 )
-pushd "gradle\bootstrap"
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
+for /F "tokens=*" %%A in (dirs) do (
+    call :GRADLE %%A || goto :ERROR
 )
-popd
 
-pushd oom
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd langur
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-
-pushd common
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd identity
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd oauth
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd catalog
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd rating
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd payment
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd billing
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd cart
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd drm
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd entitlement
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd ewallet
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd fulfilment
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd order
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd subscription
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-pushd "bootstrap"
-call gradle %gradle_arg%
-if %errorlevel% == 1 (
-	popd
-	goto END
-)
-popd
-
-goto :END
-
-:END
 echo Build done
+goto :EOF
+
+:ERROR
+popd
+echo Build error
+exit /b 1
+
+:GRADLE
+pushd %1
+call gradle %gradle_arg% || goto :ERROR
+popd %1
+goto :EOF
+
