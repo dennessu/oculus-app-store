87225fd76b160d396566fbfa1254595745f90d48
HaiweiLi <HaiweiLi@wan-san.com>
2014-03-12 18:43:41 +0800

    store ui done

--- sampleUI/store-site/routes/payment.js
+++ sampleUI/store-site/routes/payment.js
@@ -6,3 +6,11 @@ exports.Entry = function(req, res){
 exports.Create = function(req, res){
     res.render('payment/create', {layout: 'payment/layout', title: 'Store Demo'});
 };
+
+exports.ShippingAddress = function(req, res){
+    res.render('payment/address', {layout: 'payment/layout', title: 'Store Demo'});
+};
+
+exports.SelectePaymentMethod = function(req, res){
+    res.render('payment/selecte_payment_method', {layout: 'payment/layout', title: 'Store Demo'});
+};
