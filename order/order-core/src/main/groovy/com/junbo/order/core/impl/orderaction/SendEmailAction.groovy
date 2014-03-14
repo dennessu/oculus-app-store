ea74cc2c5090523478f5b438e6455ba883d4ef77
LinYiRoger <yilin@wan-san.com>
2014-03-14 18:04:43 +0800

    add send email action

--- order/order-core/src/main/groovy/com/junbo/order/core/impl/orderaction/SendEmailAction.groovy
+++ order/order-core/src/main/groovy/com/junbo/order/core/impl/orderaction/SendEmailAction.groovy
@@ -1,5 +1,7 @@
 package com.junbo.order.core.impl.orderaction
 
+import com.junbo.catalog.spec.model.offer.Offer
+import com.junbo.identity.spec.model.user.User
 import com.junbo.langur.core.promise.Promise
 import com.junbo.langur.core.webflow.action.Action
 import com.junbo.langur.core.webflow.action.ActionContext
@@ -7,6 +9,7 @@ import com.junbo.langur.core.webflow.action.ActionResult
 import com.junbo.order.clientproxy.catalog.CatalogFacade
 import com.junbo.order.clientproxy.email.EmailFacade
 import com.junbo.order.clientproxy.identity.IdentityFacade
+import com.junbo.order.spec.model.OrderItem
 import groovy.transform.CompileStatic
 
 import javax.annotation.Resource
@@ -26,7 +29,18 @@ class SendEmailAction implements Action {
     CatalogFacade catalogFacade
 
     @Override
-    Promise<ActionResult> execute(ActionContext context) {
-        return null
+    Promise<ActionResult> execute(ActionContext actionContext) {
+        def context = ActionUtils.getOrderActionContext(actionContext)
+        def order = context.orderServiceContext.order
+        def offerIds = []
+        order.orderItems.each { OrderItem item ->
+            offerIds << item.offer
+        }
+
+        return catalogFacade.getOffers(offerIds).then { List<Offer> offers ->
+             identityFacade.getUser(order.user.value).then { User user ->
+                emailFacade.sendOrderConfirmationEMail(order, user, offers)
+             }
+        }
     }
 }
