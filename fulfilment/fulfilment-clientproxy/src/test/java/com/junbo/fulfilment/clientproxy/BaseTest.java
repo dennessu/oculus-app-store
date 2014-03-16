55c14c149d8ed78bc991229ea0c18edf7d710d2f
sharkmao <sharkmao@wan-san.com>
2014-03-16 14:47:49 +0800

    fulfilment integrated with entitlement

---
+++ fulfilment/fulfilment-clientproxy/src/test/java/com/junbo/fulfilment/clientproxy/BaseTest.java
@@ -0,0 +1,8 @@
+package com.junbo.fulfilment.clientproxy;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@ContextConfiguration(locations = {"classpath:spring/context-test.xml"})
+public abstract class BaseTest extends AbstractTestNGSpringContextTests {
+}
---
+++ fulfilment/fulfilment-clientproxy/src/test/java/com/junbo/fulfilment/clientproxy/EntitlementGatewayTest.java
@@ -0,0 +1,35 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (C) 2014 Junbo and/or its affiliates. All rights reserved.
+ */
+package com.junbo.fulfilment.clientproxy;
+
+import com.junbo.fulfilment.spec.fusion.Entitlement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Date;
+
+/**
+ * EntitlementGatewayTest.
+ */
+public class EntitlementGatewayTest extends BaseTest {
+    @Autowired
+    private EntitlementGateway gateway;
+
+    //@Test
+    public void testBVT() {
+        Entitlement entitlement = new Entitlement();
+        entitlement.setUserId(12345L);
+        entitlement.setOfferId(99999L);
+        entitlement.setGrantDate(new Date());
+        entitlement.setGroup("TEST_GROUP");
+        entitlement.setType("TEST_TYPE");
+        entitlement.setTag("TEST_TAG");
+
+        String entitlementId = gateway.grant(entitlement);
+        Assert.assertNotNull(entitlementId, "entitlementId should not be null.");
+    }
+}
