23fb4302fb88889824fd38a7d6a0123f9e759778
baojing <baojingzuo@wan-san.com>
2014-03-17 19:26:06 +0800

    1. catalog-admin: offer detail & update initial 2. /offers and /offers/{id} return latest offer (including Design) instead of Released
    1. catalog-admin: offer detail & update initial
    2. /offers and /offers/{id} return latest offer (including Design)
    instead of Released

--- catalog/catalog-admin/api-proxy/offers.js
+++ catalog/catalog-admin/api-proxy/offers.js
@@ -6,6 +6,7 @@ exports.getOffers = function(req, res){
     var query = qs.stringify(req.query);
     var url = "http://localhost:8080/rest/offers";
     if (query) url = url + "?" + query;
+    console.log(url);
 
     rest.get(url).on('complete', function(result) {
         res.send(result.results);
@@ -15,11 +16,27 @@ exports.getOffers = function(req, res){
 exports.getOffer = function(req, res){
     console.log("get offer");
     var query = qs.stringify(req.query);
+    console.log(query);
     var url = "http://localhost:8080/rest/offers/" + req.params.id;
     if (query) url = url + "?" + query;
     console.log(url);
 
     rest.get(url).on('complete', function(result) {
+        if (result instanceof Error) {
+            console.log('Error:', result.message);
+            res.send(result.message);
+        } else {
+            res.send(result);
+        }
+    });
+};
+
+exports.updateOffer = function(req, res){
+    console.log("update offer");
+    var url = "http://localhost:8080/rest/offers/" + req.params.id;
+    console.log(url);
+    console.log(req.body);
+    rest.putJson(url, req.body).on('complete', function(result) {
         if (result instanceof Error) {
             console.log('Error:', result.message);
             res.send(result.message);
