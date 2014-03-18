d9970b0b211912c1042cc00424bc6ac58cbd9776
sharkmao <sharkmao@wan-san.com>
2014-03-18 15:03:00 +0800

    Merge pull request #216 from nanxin/master
    ewallet rest layer phase 2;

--- ewallet/ewallet-spec/src/main/groovy/com/junbo/ewallet/spec/model/Transaction.groovy
+++ ewallet/ewallet-spec/src/main/groovy/com/junbo/ewallet/spec/model/Transaction.groovy
@@ -6,16 +6,23 @@
 
 package com.junbo.ewallet.spec.model
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.junbo.common.jackson.annotation.OfferId
 import groovy.transform.CompileStatic
 
 /**
  * Transaction Entity. Only for response.
  */
 @CompileStatic
+@JsonPropertyOrder(['type', 'amount', 'offerId', 'createdBy', 'createdTime'])
+@JsonInclude(JsonInclude.Include.NON_NULL)
 class Transaction {
-    Long walletId
     String type
     BigDecimal amount
+    @OfferId
+    @JsonProperty('offer')
     Long offerId
     Date createdTime
     String createdBy
