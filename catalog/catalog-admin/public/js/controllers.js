1582d1f5483819a88461987b0749268a9d2cb709
baojing <baojingzuo@wan-san.com>
2014-03-19 17:53:37 +0800

    catalog-admin: developer & admin UI

--- catalog/catalog-admin/public/js/controllers.js
+++ catalog/catalog-admin/public/js/controllers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* Controllers */
-var app = angular.module('catalogAdmin.controllers', []);
+var app = angular.module('catalog.controllers', []);
 
 // Clear browser cache (in development mode)
 //
@@ -25,14 +25,60 @@ app.controller('OfferListCtrl', ['$scope', 'OffersFactory', '$location',
   	  $scope.offers = OffersFactory.query();
   }]);
 
-app.controller('OfferDetailCtrl', ['$scope', 'OfferFactory', '$routeParams', '$location',
-    function($scope, OfferFactory, $routeParams, $location) {
+app.controller('OfferReviewListCtrl', ['$scope', 'OffersFactory', '$location',
+    function($scope, OffersFactory, $location) {
+        $scope.createOffer = function () {
+            OffersFactory.create($scope.offer, function(offer){
+                $location.path('/offers/' + offer.self.id);
+            });
+        };
+        $scope.cancel = function () {
+            $location.path('/offers');
+        };
+        $scope.offers = OffersFactory.query({status: 'PendingReview'});
+    }]);
+
+app.controller('OfferResponseCtrl', ['$scope', '$routeParams', 'OfferResponse',
+    function($scope, $routeParams, OfferResponse) {
+        $scope.response = OfferResponse.data;
+        $scope.offerId = $routeParams.id;
+    }]);
+
+app.controller('OfferDetailCtrl', ['$scope', 'OfferFactory', '$routeParams', '$location','OfferResponse',
+    function($scope, OfferFactory, $routeParams, $location, OfferResponse) {
         console.log("OfferDetailCtrl");
         console.log($routeParams);
 
         $scope.updateOffer = function () {
-            OfferFactory.update({id: $routeParams.id}, $scope.offer);
-            $location.path('/offers/' + $routeParams.id);
+            OfferFactory.update({id: $routeParams.id}, $scope.offer, function(offer){
+                OfferResponse.data = "Offer updated successfully!";
+                $location.path('/offers/' + $routeParams.id + '/response');
+            });
+        };
+
+        $scope.releaseOffer = function () {
+            $scope.offer.status="Released";
+            OfferFactory.update({id: $routeParams.id}, $scope.offer, function(offer){
+                OfferResponse.data = "Offer released successfully!";
+                $location.path('/offers/' + $routeParams.id + '/response');
+            });
+        };
+
+        $scope.reviewOffer = function () {
+            $scope.offer.status="PendingReview";
+
+            OfferFactory.update({id: $routeParams.id}, $scope.offer, function(offer){
+                OfferResponse.data = "Offer submitted for review!";
+                $location.path('/offers/' + $routeParams.id + '/response');
+            });
+        };
+
+        $scope.rejectOffer = function () {
+            $scope.offer.status="Rejected";
+            OfferFactory.update({id: $routeParams.id}, $scope.offer, function(offer){
+                OfferResponse.data = "Offer rejected successfully!";
+                $location.path('/offers/' + $routeParams.id + '/response');
+            });
         };
 
         $scope.cancel = function () {
--- catalog/catalog-admin/public/js/filters.js
+++ catalog/catalog-admin/public/js/filters.js
@@ -2,7 +2,7 @@
 
 /* Filters */
 
-angular.module('catalogAdmin.filters', []).
+angular.module('catalog.filters', []).
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
