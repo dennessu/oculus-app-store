24b365b333db0a7d89242edf5b82db9eb59fb487
baojing <baojingzuo@wan-san.com>
2014-03-19 19:24:55 +0800

    catalog-admin: attribute

--- catalog/catalog-admin/public/admin/app.js
+++ catalog/catalog-admin/public/admin/app.js
@@ -2,18 +2,24 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('catalogAdmin', [
+var app = angular.module('catalogAdmin', [
         'ngRoute',
         'catalog.filters',
         'catalog.services',
         'catalog.directives',
         'catalog.controllers'
-    ]).
-    config(['$routeProvider', function($routeProvider) {
+    ]);
+
+app.config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/offers', {templateUrl: '../views/offer-list.html', controller: 'OfferListCtrl'});
         $routeProvider.when('/offers/review', {templateUrl: '../views/offer-list.html', controller: 'OfferReviewListCtrl'});
         $routeProvider.when('/offers/:id', {templateUrl: '../views/offer-detail-admin.html', controller: 'OfferDetailCtrl'});
         $routeProvider.when('/offers/:id/response', {templateUrl: '../views/offer-response.html', controller: 'OfferResponseCtrl'})
+
+        $routeProvider.when('/attributes', {templateUrl: '../views/attribute-list.html', controller: 'AttributeListCtrl'});
+        $routeProvider.when('/attributes/creation', {templateUrl: '../views/attribute-creation.html', controller: 'AttributeListCtrl'});
+        $routeProvider.when('/attributes/:id', {templateUrl: '../views/attribute-detail.html', controller: 'AttributeDetailCtrl'});
+
         $routeProvider.when('/view2', {templateUrl: '../views/partial2.html', controller: 'MyCtrl2'});
         $routeProvider.otherwise({redirectTo: '/offers/review'});
     }]);
