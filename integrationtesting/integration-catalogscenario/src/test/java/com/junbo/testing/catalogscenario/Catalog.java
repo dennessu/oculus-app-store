d0e9be76b077a8edbd9f4afb7c240e0d5f7fde11
jasonfuChengye <jasonfu@serviceCommerce.com>
2014-03-21 18:20:21 +0800

    fix code issue

--- integrationtesting/integration-catalogscenario/src/test/java/com/junbo/testing/catalogscenario/Catalog.java
+++ integrationtesting/integration-catalogscenario/src/test/java/com/junbo/testing/catalogscenario/Catalog.java
@@ -5,14 +5,17 @@
  */
 package com.junbo.testing.catalogscenario;
 
+import com.junbo.catalog.spec.model.attribute.Attribute;
 import com.junbo.testing.catalogscenario.util.BaseTestClass;
-import com.junbo.catalog.spec.model.common.ResultList;
 import com.junbo.catalog.spec.model.item.Item;
+import com.junbo.testing.common.apihelper.catalog.AttributeService;
 import com.junbo.testing.common.apihelper.catalog.ItemService;
 import com.junbo.testing.common.apihelper.catalog.OfferService;
+import com.junbo.testing.common.apihelper.catalog.impl.AttributeServiceImpl;
 import com.junbo.testing.common.apihelper.catalog.impl.ItemServiceImpl;
 import com.junbo.testing.common.apihelper.catalog.impl.OfferServiceImpl;
 import com.junbo.testing.common.blueprint.Master;
+import com.junbo.testing.common.libs.EnumHelper;
 import com.junbo.testing.common.libs.LogHelper;
 import com.junbo.testing.common.property.*;
 
@@ -37,6 +40,39 @@ public class Catalog extends BaseTestClass {
             component = Component.Catalog,
             owner = "JasonFu",
             status = Status.Incomplete,
+            description = "Test Attribute scenarios",
+            steps = {
+                    ""
+            }
+    )
+    @Test
+    public void testAttributeManagement() throws Exception {
+
+        HashMap<String, String> paraMap = new HashMap();
+        paraMap.put(EnumHelper.CatalogAttributeType.Type.toString(), "Digital");
+
+        AttributeService attributeServiceAPI = AttributeServiceImpl.instance();
+
+        Attribute attribute = new Attribute();
+        attribute.setName("Digital type");
+        attribute.setType("Digital");
+
+        String attributeId = attributeServiceAPI.postAttribute(attribute);
+        Assert.assertNotNull(attributeId);
+
+        String attributeGetId = attributeServiceAPI.getAttribute(attributeId);
+        Assert.assertNotNull(Master.getInstance().getAttribute(attributeGetId));
+
+        List<String> attributeResultList = attributeServiceAPI.getAttribute(paraMap);
+        Assert.assertNotNull(attributeResultList);
+    }
+
+    @Property(
+            priority = Priority.BVT,
+            features = "CatalogScenarios",
+            component = Component.Catalog,
+            owner = "JasonFu",
+            status = Status.Incomplete,
             description = "Test Item scenarios",
             steps = {
                     ""
@@ -46,19 +82,22 @@ public class Catalog extends BaseTestClass {
     public void testItemManagement() throws Exception {
 
         HashMap<String, String> paraMap = new HashMap();
-        paraMap.put("status", "Design");
+        paraMap.put("status", EnumHelper.CatalogEntityStatus.Design.toString());
 
         ItemService itemServiceAPI = ItemServiceImpl.instance();
 
-        String itemId = itemServiceAPI.postDefaultItem();
+        String itemId = itemServiceAPI.postDefaultItem(true);
         Assert.assertNotNull(itemId);
 
-        List<String> itemResultList = itemServiceAPI.getItem(paraMap);
-        Assert.assertNotNull(itemResultList);
+        itemId = itemServiceAPI.postDefaultItem(false);
+        Assert.assertNotNull(itemId);
 
         Item itemGet = Master.getInstance().getItem(itemServiceAPI.getItem(itemId, paraMap));
         Assert.assertNotNull(itemGet);
-    }
+
+        List<String> itemResultList = itemServiceAPI.getItem(paraMap);
+        Assert.assertNotNull(itemResultList);
+}
 
     @Property(
             priority = Priority.BVT,
@@ -75,12 +114,11 @@ public class Catalog extends BaseTestClass {
     public void testOfferManagement() throws Exception {
 
         HashMap<String, String> paraMap = new HashMap();
-        paraMap.put("status", "Design");
+        paraMap.put("status", EnumHelper.CatalogEntityStatus.Design.toString());
 
         OfferService offerServiceAPI = OfferServiceImpl.instance();
 
         String offerId = offerServiceAPI.postDefaultOffer();
         Assert.assertNotNull(offerId);
-
     }
 }
