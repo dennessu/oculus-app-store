57c6657025aef5ff0e59c54b2445ea0aced3dba5
yunlongzhao <yunlongzhao@wan-san.com>
2014-03-21 17:51:48 +0800

    update common methods

---
+++ integrationtesting/integration-common/src/main/java/com/junbo/testing/common/apihelper/billing/BillingService.java
@@ -0,0 +1,15 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (C) 2014 Junbo and/or its affiliates. All rights reserved.
+ */
+package com.junbo.testing.common.apihelper.billing;
+
+/**
+ * Created by Yunlong on 3/21/14.
+ */
+public class BillingService {
+    private BillingService() {
+    }
+
+}
