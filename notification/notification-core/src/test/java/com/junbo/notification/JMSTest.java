ae23adc4d2ff672d1a288f8359520ba3f9843b22
sharkmao <sharkmao@wan-san.com>
2014-03-25 23:41:55 +0800

    add notification transactional case

--- notification/notification-core/src/test/java/com/junbo/notification/JMSTest.java
+++ notification/notification-core/src/test/java/com/junbo/notification/JMSTest.java
@@ -1,13 +1,18 @@
 package com.junbo.notification;
 
 import com.junbo.notification.queue.TestProducer;
+import com.junbo.notification.retry.TransactionalListener;
 import com.junbo.notification.topic.TestPublisher;
 import org.apache.activemq.broker.BrokerService;
+import org.junit.BeforeClass;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @ContextConfiguration(locations = {"classpath:spring/context-test-*.xml"})
 public class JMSTest extends AbstractTestNGSpringContextTests {
@@ -15,10 +20,20 @@ public class JMSTest extends AbstractTestNGSpringContextTests {
     private BrokerService broker;
 
     @Autowired
+    @Qualifier("testProducer")
     private TestProducer producer;
 
     @Autowired
+    @Qualifier("testProducer2")
+    private TestProducer producer2;
+
+    @Autowired
     private TestPublisher publisher;
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        broker.setSystemExitOnShutdown(true);
+    }
 
     @AfterClass
     public void tearDown() throws Exception {
@@ -33,5 +48,13 @@ public class JMSTest extends AbstractTestNGSpringContextTests {
     @Test
     public void testTopic() {
         publisher.send("come on baby!");
+    }
+
+    @Test
+    public void testTransactionalListener() throws Exception {
+        TransactionalListener.FAILED_COUNT = new AtomicInteger(0);
+        producer2.send("come on failure!");
+
+        Thread.sleep(5000);
     }
 }
