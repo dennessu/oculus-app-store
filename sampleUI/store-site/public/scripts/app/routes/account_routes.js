30f3b03996f662b459dabe029111724070c2a1ec
HaiweiLi <HaiweiLi@wan-san.com>
2014-03-26 23:58:25 +0800

    manage payment

--- sampleUI/store-site/public/scripts/app/routes/account_routes.js
+++ sampleUI/store-site/public/scripts/app/routes/account_routes.js
@@ -97,5 +97,33 @@ var AccountRoutes = {
         beforeModel: function(){
             Utils.GetViews(AppConfig.Templates.Account.Payment);
         }
+    }),
+    AddPaymentRoute: Ember.Route.extend({
+        beforeModel: function(){
+            Utils.GetViews(AppConfig.Templates.Account.AddPayment);
+        },
+        setupController:function(controller, model){
+            var identityProvider = new IdentityProvider();
+            identityProvider.GetProfile(Utils.GenerateRequestModel(null), function(resultData){
+                if(resultData.data.status == 200){
+                    var birthday = new Date(JSON.parse(resultData.data.data).dateOfBirth);
+                    var nowYear = new Date().getFullYear();
+                    var age = nowYear - birthday.getFullYear();
+                    console.log("birthday Years:", birthday.getFullYear(), " Now Years:", nowYear);
+                    if(age <= 17){
+                        console.log("Set isHolder is True");
+                        controller.set("isHolder", true);
+                    }else{
+                        console.log("Set isHolder is False");
+                        controller.set("isHolder", false);
+                    }
+                }else{
+                    console.log("Set isHolder is False");
+                    controller.set("isHolder", false);
+                }
+            });
+        }
     })
+
+
 };
