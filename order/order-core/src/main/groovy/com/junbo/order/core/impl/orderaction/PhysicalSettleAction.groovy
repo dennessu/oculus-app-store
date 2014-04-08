053b15e2a5daeeb57d79ded275a7e5ca389515ea
LinYiRoger <yilin@silkcloud.com>
2014-04-08 12:46:44 +0800

    add settle action for physical goods

---
+++ order/order-core/src/main/groovy/com/junbo/order/core/impl/orderaction/PhysicalSettleAction.groovy
@@ -0,0 +1,63 @@
+package com.junbo.order.core.impl.orderaction
+
+import com.junbo.billing.spec.enums.BalanceType
+import com.junbo.billing.spec.model.Balance
+import com.junbo.langur.core.promise.Promise
+import com.junbo.langur.core.webflow.action.ActionContext
+import com.junbo.langur.core.webflow.action.ActionResult
+import com.junbo.order.clientproxy.FacadeContainer
+import com.junbo.order.core.annotation.OrderEventAwareAfter
+import com.junbo.order.core.annotation.OrderEventAwareBefore
+import com.junbo.order.core.impl.common.BillingEventBuilder
+import com.junbo.order.core.impl.common.CoreBuilder
+import com.junbo.order.core.impl.common.CoreUtils
+import com.junbo.order.core.impl.order.OrderServiceContextBuilder
+import com.junbo.order.db.repo.OrderRepository
+import com.junbo.order.spec.error.AppErrors
+import groovy.transform.CompileStatic
+import groovy.transform.TypeChecked
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.transaction.annotation.Transactional
+
+/**
+ * Settle Action for Physical Goods.
+ */
+@CompileStatic
+@TypeChecked
+class PhysicalSettleAction extends BaseOrderEventAwareAction {
+    @Autowired
+    @Qualifier('orderFacadeContainer')
+    FacadeContainer facadeContainer
+    @Autowired
+    OrderRepository orderRepository
+    @Autowired
+    OrderServiceContextBuilder orderServiceContextBuilder
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PhysicalSettleAction)
+
+    @Override
+    @OrderEventAwareBefore(action = 'PhysicalSettleAction')
+    @OrderEventAwareAfter(action = 'PhysicalSettleAction')
+    @Transactional
+    Promise<ActionResult> execute(ActionContext actionContext) {
+        def context = ActionUtils.getOrderActionContext(actionContext)
+        Balance balance = CoreBuilder.buildBalance(context.orderServiceContext.order, BalanceType.DEBIT)
+        Promise promise = facadeContainer.billingFacade.createBalance(balance)
+        promise.syncRecover {  Throwable throwable ->
+            LOGGER.error('name=Order_PhysicalSettle_Error', throwable)
+            context.orderServiceContext.order.tentative = true
+            throw AppErrors.INSTANCE.
+                    billingConnectionError(CoreUtils.toAppErrors(throwable)).exception()
+        }.then { Balance resultBalance ->
+            def billingEvent = BillingEventBuilder.buildBillingEvent(resultBalance)
+            orderRepository.createBillingEvent(context.orderServiceContext.order.id.value, billingEvent)
+            orderServiceContextBuilder.refreshBalances(context.orderServiceContext).syncThen {
+                // TODO: save order level tax
+                return CoreBuilder.buildActionResultForOrderEventAwareAction(context, billingEvent.status)
+            }
+        }
+    }
+}
