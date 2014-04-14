47cdf388fe62c565206938c381b664d44a5c4d53
kgdev <kg@wan-san.com>
2014-04-14 16:08:50 +0800

    update langur xsd

---
+++ langur/langur-processor/src/test/groovy/com/junbo/common/error/AppErrorException.java
@@ -0,0 +1,24 @@
+package com.junbo.common.error;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Shenhua on 4/14/2014.
+ */
+public class AppErrorException extends WebApplicationException {
+    private final AppError error;
+
+    public AppError getError() {
+        return error;
+    }
+
+    public AppErrorException(AppError error) {
+        super(Response.status(error.getHttpStatusCode()).entity(new Error(error.getCode(),
+                error.getDescription(), error.getField(), null)).
+                type(MediaType.APPLICATION_JSON_TYPE).build());
+
+        this.error = error;
+    }
+}
