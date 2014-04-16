c73f0b6cb66f13ffc7e1c53d4cc47f848b27324e
kgdev <kg@wan-san.com>
2014-04-16 10:27:23 +0800

    log every errors.

--- common/common-lib/src/main/java/com/junbo/common/error/RestExceptionMapper.java
+++ common/common-lib/src/main/java/com/junbo/common/error/RestExceptionMapper.java
@@ -5,10 +5,15 @@
  */
 package com.junbo.common.error;
 
+import org.glassfish.jersey.server.monitoring.ApplicationEvent;
+import org.glassfish.jersey.server.monitoring.ApplicationEventListener;
+import org.glassfish.jersey.server.monitoring.RequestEvent;
+import org.glassfish.jersey.server.monitoring.RequestEventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.CompletionCallback;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -17,20 +22,34 @@ import javax.ws.rs.ext.Provider;
  * Javadoc.
  */
 @Provider
-public class RestExceptionMapper implements ExceptionMapper<Exception> {
+public class RestExceptionMapper implements ExceptionMapper<Exception>, ApplicationEventListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RestExceptionMapper.class);
 
     @Override
     public Response toResponse(Exception exception) {
 
-        LOGGER.error("Log unhandled exception", exception);
-
         if (exception instanceof WebApplicationException) {
             return ((WebApplicationException) exception).getResponse();
         }
 
         return ERRORS.internalServerError().exception().getResponse();
+    }
+
+    @Override
+    public void onEvent(ApplicationEvent event) {
+    }
+
+    @Override
+    public RequestEventListener onRequest(RequestEvent requestEvent) {
+        return new RequestEventListener() {
+            @Override
+            public void onEvent(RequestEvent event) {
+                if (event.getType() == RequestEvent.Type.ON_EXCEPTION) {
+                    LOGGER.error("Log unhandled exception: ", event.getException());
+                }
+            }
+        };
     }
 
     public static final Errors ERRORS = ErrorProxy.newProxyInstance(Errors.class);
