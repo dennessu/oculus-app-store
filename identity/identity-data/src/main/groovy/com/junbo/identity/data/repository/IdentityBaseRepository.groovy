cf84a92e2cb2115c1e8d74b4d12330bf0459cf1e
haomin <haomin@wan-san.com>
2014-04-18 19:46:46 +0800

    fix build error

--- identity/identity-data/src/main/groovy/com/junbo/identity/data/repository/IdentityBaseRepository.groovy
+++ identity/identity-data/src/main/groovy/com/junbo/identity/data/repository/IdentityBaseRepository.groovy
@@ -1,8 +1,5 @@
 package com.junbo.identity.data.repository
 
-import com.junbo.common.id.UserPasswordId
-import com.junbo.identity.spec.model.users.UserPassword
-import com.junbo.identity.spec.v1.option.list.UserPasswordListOptions
 import com.junbo.langur.core.promise.Promise
 
 /**
--- identity/identity-data/src/main/groovy/com/junbo/identity/data/repository/impl/PersistentMode.groovy
+++ identity/identity-data/src/main/groovy/com/junbo/identity/data/repository/impl/PersistentMode.groovy
@@ -6,7 +6,7 @@ import groovy.transform.CompileStatic
  * Created by haomin on 14-4-18.
  */
 @CompileStatic
-public enum PersistentMode {
+enum PersistentMode {
     CLOUDANT_READ_WRITE,
     CLOUDANT_READ_DUAL_WRITE_CLOUDANT_PRIMARY,
     CLOUDANT_READ_DUAL_WRITE_SQL_PRIMARY
