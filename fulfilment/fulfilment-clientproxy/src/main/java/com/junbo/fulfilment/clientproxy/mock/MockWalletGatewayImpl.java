cdaa9e99f6537d2535fe66eb949e6b5a7d8518bf
sharkmao <sharkmao@silkcloud.com>
2014-04-18 14:19:46 +0800

    change according to ewallet change

--- fulfilment/fulfilment-clientproxy/src/main/java/com/junbo/fulfilment/clientproxy/mock/MockWalletGatewayImpl.java
+++ fulfilment/fulfilment-clientproxy/src/main/java/com/junbo/fulfilment/clientproxy/mock/MockWalletGatewayImpl.java
@@ -7,26 +7,17 @@ package com.junbo.fulfilment.clientproxy.mock;
 
 import com.junbo.ewallet.spec.model.CreditRequest;
 import com.junbo.ewallet.spec.model.Transaction;
-import com.junbo.ewallet.spec.model.Wallet;
 import com.junbo.fulfilment.clientproxy.WalletGateway;
-
-import java.util.ArrayList;
 
 /**
  * MockWalletGatewayImpl.
  */
 public class MockWalletGatewayImpl implements WalletGateway {
     @Override
-    public Wallet credit(CreditRequest request) {
-        Wallet wallet = new Wallet();
+    public Transaction credit(CreditRequest request) {
+        Transaction trx = new Transaction();
+        trx.setTransactionId(System.currentTimeMillis());
 
-        final Transaction trx = new Transaction();
-        trx.setTransactionId(12345L);
-
-        wallet.setTransactions(new ArrayList<Transaction>() {{
-            add(trx);
-        }});
-
-        return wallet;
+        return trx;
     }
 }
