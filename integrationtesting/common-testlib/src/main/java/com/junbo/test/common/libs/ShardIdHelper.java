db81fc391293ef1b12d7b24fceceabd67c9b7fd2
yunlongzhao <yunlongzhao@wan-san.com>
2014-04-23 12:12:17 +0800

    fix assert import and add shardIdHelper for DB

---
+++ integrationtesting/common-testlib/src/main/java/com/junbo/test/common/libs/ShardIdHelper.java
@@ -0,0 +1,29 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (C) 2014 Junbo and/or its affiliates. All rights reserved.
+ */
+package com.junbo.test.common.libs;
+
+import com.junbo.common.id.UserId;
+import com.junbo.sharding.*;
+import com.junbo.test.common.exception.TestException;
+
+/**
+ * Created by Yunlong on 4/23/14.
+ */
+public class ShardIdHelper {
+
+    public ShardIdHelper() {
+    }
+
+    public static int getShardIdByUid(String uid) {
+        DefaultShardAlgorithm shardAlgorithm = new DefaultShardAlgorithm();
+        if (uid != null && !uid.isEmpty()) {
+            return shardAlgorithm.shardId(IdConverter.hexStringToId(UserId.class, uid));
+        } else {
+            throw new TestException("user Id can't be empty!");
+        }
+    }
+
+}
