988887b0768afeb15f96070da42c37b2b2faf6c9
NanXin <nanxin@silkcloud.com>
2014-04-25 17:28:32 +0800

    refine rev impl;

--- entitlement/entitlement-core/src/main/java/com/junbo/entitlement/core/service/BaseService.java
+++ entitlement/entitlement-core/src/main/java/com/junbo/entitlement/core/service/BaseService.java
@@ -63,7 +63,6 @@ public class BaseService {
     protected void fillUpdate(Entitlement entitlement, Entitlement existingEntitlement) {
         existingEntitlement.setUseCount(entitlement.getUseCount());
         existingEntitlement.setExpirationTime(entitlement.getExpirationTime());
-        existingEntitlement.setRev(entitlement.getRev());
         if (entitlement.getIsBanned() != null) {
             existingEntitlement.setIsBanned(entitlement.getIsBanned());
         } else {
@@ -98,10 +97,11 @@ public class BaseService {
 
     protected void validateUpdate(Entitlement entitlement, Entitlement existingEntitlement) {
         checkOauth(existingEntitlement);
-        validateEquals(formatId(existingEntitlement.getUserId()), formatId(entitlement.getUserId()), "user");
-        validateEquals(formatId(existingEntitlement.getEntitlementDefinitionId()),
-                formatId(entitlement.getEntitlementDefinitionId()), "definition");
-        validateEquals(existingEntitlement.getGrantTime(), entitlement.getGrantTime(), "grantTime");
+        validateEquals(entitlement.getRev(), existingEntitlement.getRev(), "rev");
+        validateEquals(formatId(entitlement.getUserId()), formatId(existingEntitlement.getUserId()), "user");
+        validateEquals(formatId(entitlement.getEntitlementDefinitionId()),
+                formatId(existingEntitlement.getEntitlementDefinitionId()), "definition");
+        validateEquals(entitlement.getGrantTime(), existingEntitlement.getGrantTime(), "grantTime");
         if (entitlement.getUseCount() != null && entitlement.getUseCount() < 1) {
             throw AppErrors.INSTANCE.fieldNotCorrect("useCount", "useCount should not be negative").exception();
         }
