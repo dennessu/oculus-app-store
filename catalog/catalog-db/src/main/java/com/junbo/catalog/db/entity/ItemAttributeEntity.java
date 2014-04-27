40811958418cb3dd4d6c3308036ca3239729059a
baojing <baojingzuo@silkcloud.com>
2014-04-28 01:57:41 +0800

    refine attribute service

---
+++ catalog/catalog-db/src/main/java/com/junbo/catalog/db/entity/ItemAttributeEntity.java
@@ -0,0 +1,23 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (C) 2014 Junbo and/or its affiliates. All rights reserved.
+ */
+
+package com.junbo.catalog.db.entity;
+
+import com.junbo.catalog.db.dao.StringJsonUserType;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * ItemAttributeEntity.
+ */
+@Entity
+@Table(name="item_attribute")
+@TypeDefs(@TypeDef(name="json-string", typeClass=StringJsonUserType.class))
+public class ItemAttributeEntity extends AttributeEntity {
+}
