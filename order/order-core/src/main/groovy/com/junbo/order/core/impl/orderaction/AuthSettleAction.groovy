3a4fe5217ac0ee8892b1130b8da4f52e933d9ccb
fzhangtj <fzhang@silkcloud.com>
2014-04-30 14:11:21 +0800

    fix store value insufficient fund issue at order side

--- order/order-core/src/main/groovy/com/junbo/order/core/impl/orderaction/AuthSettleAction.groovy
+++ order/order-core/src/main/groovy/com/junbo/order/core/impl/orderaction/AuthSettleAction.groovy
@@ -10,7 +10,6 @@ import com.junbo.order.core.annotation.OrderEventAwareAfter
 import com.junbo.order.core.annotation.OrderEventAwareBefore
 import com.junbo.order.core.impl.common.BillingEventBuilder
 import com.junbo.order.core.impl.common.CoreBuilder
-import com.junbo.order.core.impl.common.CoreUtils
 import com.junbo.order.core.impl.internal.OrderInternalService
 import com.junbo.order.core.impl.order.OrderServiceContextBuilder
 import com.junbo.order.db.repo.OrderRepository
@@ -53,8 +52,7 @@ class AuthSettleAction extends BaseOrderEventAwareAction {
         promise.syncRecover { Throwable throwable ->
             LOGGER.error('name=Order_AuthSettle_Error', throwable)
             context.orderServiceContext.order.tentative = true
-            throw AppErrors.INSTANCE.
-                    billingConnectionError(CoreUtils.toAppErrors(throwable)).exception()
+            throw facadeContainer.billingFacade.convertError(throwable).exception()
         }.then { Balance resultBalance ->
             context.orderServiceContext.order.tentative = true
             if (resultBalance == null) {
