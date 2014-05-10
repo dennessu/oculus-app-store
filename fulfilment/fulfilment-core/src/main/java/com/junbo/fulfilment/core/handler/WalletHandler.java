f7982758c15614424830495cbd9f972de9f3bf38
sharkmao <sharkmao@silkcloud.com>
2014-05-10 13:42:52 +0800

    change wallet credit fulfilment

--- fulfilment/fulfilment-core/src/main/java/com/junbo/fulfilment/core/handler/WalletHandler.java
+++ fulfilment/fulfilment-core/src/main/java/com/junbo/fulfilment/core/handler/WalletHandler.java
@@ -9,12 +9,9 @@ import com.junbo.ewallet.spec.model.CreditRequest;
 import com.junbo.ewallet.spec.model.Transaction;
 import com.junbo.fulfilment.common.util.Constant;
 import com.junbo.fulfilment.core.context.WalletContext;
-import com.junbo.fulfilment.spec.fusion.LinkedEntry;
 import com.junbo.fulfilment.spec.model.FulfilmentAction;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -24,29 +21,22 @@ import java.util.UUID;
 public class WalletHandler extends HandlerSupport<WalletContext> {
     @Override
     protected Object handle(WalletContext context, FulfilmentAction action) {
-        List<Long> results = new ArrayList<>();
+        Map<String, Object> actionProp = action.getProperties();
 
-        for (LinkedEntry itemEntry : action.getItems()) {
-            Map<String, Object> actionProp = action.getProperties();
+        CreditRequest request = new CreditRequest();
 
-            CreditRequest request = new CreditRequest();
+        request.setTrackingUuid(UUID.randomUUID());
+        request.setUserId(context.getUserId());
+        request.setCurrency(actionProp.get(Constant.STORED_VALUE_CURRENCY).toString());
 
-            request.setTrackingUuid(UUID.randomUUID());
-            request.setUserId(context.getUserId());
-            request.setCurrency(actionProp.get(Constant.STORED_VALUE_CURRENCY).toString());
+        // aggregate credit amount
+        BigDecimal amount = (BigDecimal) actionProp.get(Constant.STORED_VALUE_AMOUNT);
+        BigDecimal totalCreditAmount = amount
+                .multiply(new BigDecimal(action.getCopyCount()));
 
-            // aggregate credit amount
-            BigDecimal amount = (BigDecimal) actionProp.get(Constant.STORED_VALUE_AMOUNT);
-            BigDecimal totalCreditAmount = amount
-                    .multiply(new BigDecimal(action.getCopyCount()))
-                    .multiply(new BigDecimal(itemEntry.getQuantity()));
+        request.setAmount(totalCreditAmount);
+        Transaction transaction = walletGateway.credit(request);
 
-            request.setAmount(totalCreditAmount);
-
-            Transaction transaction = walletGateway.credit(request);
-            results.add(transaction.getTransactionId());
-        }
-
-        return results;
+        return transaction.getTransactionId();
     }
 }
