02f84bc155b6d2623bdae25f651420da6eee9a3b
liangfuxia <liangfuxia@wan-san.com>
2014-05-14 12:28:11 +0800

    Make cryptoMessage object; enable identity start crypto service

---
+++ crypto/crypto-core/src/main/groovy/com/junbo/crypto/core/validator/impl/CryptoMessageValidatorImpl.groovy
@@ -0,0 +1,94 @@
+package com.junbo.crypto.core.validator.impl
+
+import com.junbo.common.id.UserId
+import com.junbo.crypto.core.validator.CryptoMessageValidator
+import com.junbo.crypto.spec.error.AppErrors
+import com.junbo.crypto.spec.model.CryptoMessage
+import com.junbo.identity.spec.v1.model.User
+import com.junbo.identity.spec.v1.option.model.UserGetOptions
+import com.junbo.identity.spec.v1.resource.UserResource
+import com.junbo.langur.core.promise.Promise
+import groovy.transform.CompileStatic
+import org.springframework.beans.factory.annotation.Required
+import org.springframework.util.StringUtils
+
+/**
+ * Created by liangfu on 5/14/14.
+ */
+@CompileStatic
+class CryptoMessageValidatorImpl implements CryptoMessageValidator {
+
+    private UserResource userResource
+
+    private String versionSeparator
+
+    @Override
+    Promise<Void> validateEncrypt(UserId userId, CryptoMessage rawMessage) {
+        if (userId == null) {
+            throw new IllegalArgumentException('userId is null')
+        }
+        if (rawMessage == null) {
+            throw new IllegalArgumentException('rawMessage is null')
+        }
+        if (rawMessage.value == null) {
+            throw AppErrors.INSTANCE.fieldMissing('value').exception()
+        }
+
+        return userResource.get(userId, new UserGetOptions()).then { User user ->
+            if (user == null) {
+                throw AppErrors.INSTANCE.userNotFound(userId).exception()
+            }
+
+            return Promise.pure(null)
+        }
+    }
+
+    @Override
+    Promise<Void> validateDecrypt(UserId userId, CryptoMessage decryptMessage) {
+        if (userId == null) {
+            throw new IllegalArgumentException('userId is null')
+        }
+        if (decryptMessage == null) {
+            throw new IllegalArgumentException('decryptMessage is null')
+        }
+        if (decryptMessage.value == null) {
+            throw AppErrors.INSTANCE.fieldMissing('value').exception()
+        }
+
+        String[] messageInfo = decryptMessage.value.split(versionSeparator)
+        if (messageInfo == null || messageInfo.length != 2 || !(isValidInteger(messageInfo[0]))) {
+            throw AppErrors.INSTANCE.fieldInvalid('EncryptMessage must be in the format as versionNumber' +
+                    versionSeparator + 'encryptValue').exception()
+        }
+
+        return Promise.pure(null)
+    }
+
+    private boolean isValidInteger(String s) {
+        if (StringUtils.isEmpty(s)) {
+            return false
+        }
+
+        Integer convertedInt = null
+        try {
+            convertedInt = Integer.parseInt(s)
+        } catch (Exception e) {
+            return false
+        }
+
+        if (convertedInt > 0) {
+            return true
+        }
+        return false
+    }
+
+    @Required
+    void setUserResource(UserResource userResource) {
+        this.userResource = userResource
+    }
+
+    @Required
+    void setVersionSeparator(String versionSeparator) {
+        this.versionSeparator = versionSeparator
+    }
+}
