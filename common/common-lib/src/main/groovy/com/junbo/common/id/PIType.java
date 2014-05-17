66f096527deb7e93b0ee72c5b4bc3b6ad1f8f1d2
minhao <haomin@silkcloud.com>
2014-05-16 18:15:14 -0700

    fix misleading pitype error message
    Summary: fix misleading pitype error message
    
    Test Plan: no
    
    Reviewers: wenhongzhu
    
    Differential Revision: https://code.silkcloud.info/D58

--- common/common-lib/src/main/groovy/com/junbo/common/id/PIType.java
+++ common/common-lib/src/main/groovy/com/junbo/common/id/PIType.java
@@ -8,6 +8,7 @@ package com.junbo.common.id;
 
 import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -40,5 +41,21 @@ public enum PIType {
     // This method can be used for reverse lookup purpose
     public static PIType get(Long id) {
         return (PIType)LOOKUP.get(id);
+    }
+
+    public static String allTypes() {
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+
+        Iterator<PIType> iter = LOOKUP.values().iterator();
+        while (iter.hasNext()) {
+            sb.append(iter.next().name());
+            if (iter.hasNext()) {
+                sb.append('|');
+            }
+        }
+        sb.append(']');
+
+        return sb.toString();
     }
 }
