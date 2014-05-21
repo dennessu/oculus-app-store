9e23a21a11e20324c24ef048a78af9be433b0a48
dw2014 <dw@wan-san.com>
2014-05-21 15:57:45 +0800

    Merge remote-tracking branch 'upstream/master'
    Conflicts:
    	integrationtesting/integration-catalogscenario/build.gradle

---
+++ integrationtesting/identity-integration/src/test/java/com/junbo/test/identity/postUserPersonalInfoLink.java
@@ -0,0 +1,61 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (C) 2014 Junbo and/or its affiliates. All rights reserved.
+ */
+package com.junbo.test.identity;
+
+import com.junbo.identity.spec.v1.model.User;
+import com.junbo.identity.spec.v1.model.UserPersonalInfo;
+import com.junbo.identity.spec.v1.model.UserPersonalInfoLink;
+import com.junbo.test.common.HttpclientHelper;
+import com.junbo.test.common.Validator;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author dw
+ */
+public class postUserPersonalInfoLink {
+
+    @BeforeMethod
+    public void setup() {
+        HttpclientHelper.CreateHttpClient();
+    }
+
+    @AfterMethod
+    public void dispose() throws Exception {
+        HttpclientHelper.CloseHttpClient();
+    }
+
+    @Test(groups = "bvt")
+    public void postUserPersonalInfoLink() throws Exception {
+        User user = Identity.UserPostDefault();
+        UserPersonalInfo posted = Identity.UserPersonalInfoPostByType(
+                user.getId(), IdentityModel.UserPersonalInfoType.EMAIL);
+        UserPersonalInfo stored = Identity.UserPersonalInfoGetByUserPersonalInfoId(posted.getId());
+        Validator.Validate("validate user personal info", posted, stored);
+        List<UserPersonalInfoLink> emails = new ArrayList<>();
+        UserPersonalInfoLink userPersonalInfoLink = new UserPersonalInfoLink();
+        userPersonalInfoLink.setUserId(user.getId());
+        userPersonalInfoLink.setIsDefault(true);
+        userPersonalInfoLink.setValue(posted.getId());
+        emails.add(userPersonalInfoLink);
+        user.setEmails(emails);
+        User updated = Identity.UserPut(user);
+        User storedUser = Identity.UserGetByUserId(user.getId());
+        Validator.Validate("validate user", updated, storedUser);
+
+        User user1 = Identity.UserPostDefault();
+        emails.clear();
+        userPersonalInfoLink.setUserId(user1.getId());
+        emails.add(userPersonalInfoLink);
+        user1.setEmails(emails);
+
+        Identity.UserPut(user1);
+    }
+}
