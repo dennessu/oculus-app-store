a7786280915068d8116171f3a09cd8b0c8e31f0b
sharkmao <sharkmao@silkcloud.com>
2014-05-21 17:17:04 +0800

    pgha scripts update

--- scripts/pgha/test/master2slave.sh
+++ scripts/pgha/test/master2slave.sh
@@ -18,18 +18,18 @@ done
 # insert against primary pgbouncer proxy
 for k in $(seq 1 50)
 do
-  $PGBIN_PATH/psql testdb -h $PRIMARY_PGBOUNCER_HOST -p $PRIMARY_PGBOUNCER_PORT -c "INSERT INTO student VALUES ($k);"
+  $PGBIN_PATH/psql testdb -h $PRIMARY_PGBOUNCER_HOST -p $PGBOUNCER_PORT -c "INSERT INTO student VALUES ($k);"
 done
 
 # insert against secondary pgbouncer proxy
 for k in $(seq 1 50)
 do
-  $PGBIN_PATH/psql testdb -h $SECONDARY_PGBOUNCER_HOST -p $SECONDARY_PGBOUNCER_PORT -c "INSERT INTO student VALUES ($k);"
+  $PGBIN_PATH/psql testdb -h $SECONDARY_PGBOUNCER_HOST -p $PGBOUNCER_PORT -c "INSERT INTO student VALUES ($k);"
 done
 
 echo 'waiting for replication...'
 sleep 5s
 
 echo 'check replication data on slave...'
-$PGBIN_PATH/psql testdb -h $SLAVE_SERVER -p $SLAVE_PORT -c "SELECT max(id) FROM student"
-$PGBIN_PATH/psql testdb -h $SLAVE_SERVER -p $SLAVE_PORT -c "SELECT count('x') FROM student"
+$PGBIN_PATH/psql testdb -h $SLAVE_HOST -p $SLAVE_DB_PORT -c "SELECT max(id) FROM student"
+$PGBIN_PATH/psql testdb -h $SLAVE_HOST -p $SLAVE_DB_PORT -c "SELECT count('x') FROM student"
