536c86de8baecbe7a7f1a0018c32da8e5853a6c1
sharkmao <sharkmao@silkcloud.com>
2014-05-21 14:22:23 +0800

    pgha scripts update

--- scripts/pgha/base_backup.sh
+++ scripts/pgha/base_backup.sh
@@ -5,8 +5,8 @@ source common.sh
 checkAccount $DEPLOYMENT_ACCOUNT
 
 echo "create backup data folder $BACKUP_PATH"
-mkdir -p $BACKUP_PATH
-rm -rf $BACKUP_PATH/*
+rm -rf $BACKUP_PATH
+mkdir $BACKUP_PATH
 
 echo "start backup master database..."
 pg_basebackup -D $BACKUP_PATH -w -R --xlog-method=stream --dbname="host=$MASTER_HOST port=$MASTER_DB_PORT user=$PGUSER"
