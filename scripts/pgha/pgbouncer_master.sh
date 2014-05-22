9c3175b9bc9d2f54f360ea029200e7fb005714dd
sharkmao <sharkmao@silkcloud.com>
2014-05-22 17:53:05 +0800

    pgha scripts update

--- scripts/pgha/pgbouncer_master.sh
+++ scripts/pgha/pgbouncer_master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source common.sh
 
-echo "primary pgbouncer is pointing to master db server"
-./pgbouncer.sh $MASTER_HOST $MASTER_DB_PORT $PRIMARY_PGBOUNCER_HOST $PRIMARY_PGBOUNCER_PORT
+echo "pgbouncer is pointing to master db server"
+./pgbouncer.sh $MASTER_HOST $MASTER_DB_PORT
--- scripts/pgha/pgbouncer_slave.sh
+++ scripts/pgha/pgbouncer_slave.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source common.sh
 
-echo "primary pgbouncer is pointing to slave db server"
-./pgbouncer.sh $SLAVE_HOST $SLAVE_DB_PORT $PRIMARY_PGBOUNCER_HOST $PRIMARY_PGBOUNCER_PORT
+echo "pgbouncer is pointing to slave db server"
+./pgbouncer.sh $SLAVE_HOST $SLAVE_DB_PORT
