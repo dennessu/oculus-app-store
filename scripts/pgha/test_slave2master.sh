6cd35f08b0dabf3f2e8ad5cd7b27b44331070587
sharkmao <sharkmao@silkcloud.com>
2014-05-22 17:53:05 +0800

    pgha scripts update

--- scripts/pgha/test_slave2master.sh
+++ scripts/pgha/test_slave2master.sh
@@ -4,32 +4,32 @@ source common.sh
 set +e
 $PGBIN_PATH/dropdb testdb -h $SLAVE_HOST -p $SLAVE_DB_PORT
 $PGBIN_PATH/createdb testdb -h $SLAVE_HOST -p $SLAVE_DB_PORT
-$PGBIN_PATH/psql testdb -h $SLAVE_HOST -p $SLAVE_DB_PORT -c "CREATE TABLE student (id bigint);"
+$PGBIN_PATH/psql testdb -h $SLAVE_HOST -p $SLAVE_DB_PORT -c "CREATE TABLE teacher (id bigint);"
 set -e
 
-$PGBIN_PATH/psql testdb -h $SLAVE_HOST -p $SLAVE_DB_PORT -c "DELETE FROM student;"
+$PGBIN_PATH/psql testdb -h $SLAVE_HOST -p $SLAVE_DB_PORT -c "DELETE FROM teacher;"
 
 # insert against SLAVE server
-for k in $(seq 1 50)
+for k in $(seq 1 49)
 do
-  $PGBIN_PATH/psql testdb -h $SLAVE_HOST -p $SLAVE_DB_PORT -c "INSERT INTO student VALUES ($k);"
+  $PGBIN_PATH/psql testdb -h $SLAVE_HOST -p $SLAVE_DB_PORT -c "INSERT INTO teacher VALUES ($k);"
 done
 
 # insert against primary pgbouncer proxy
-for k in $(seq 1 50)
+for k in $(seq 1 49)
 do
-  $PGBIN_PATH/psql testdb -h $PRIMARY_PGBOUNCER_HOST -p $PGBOUNCER_PORT -c "INSERT INTO student VALUES ($k);"
+  $PGBIN_PATH/psql testdb -h $PRIMARY_PGBOUNCER_HOST -p $PGBOUNCER_PORT -c "INSERT INTO teacher VALUES ($k);"
 done
 
 # insert against secondary pgbouncer proxy
-for k in $(seq 1 50)
+for k in $(seq 1 49)
 do
-  $PGBIN_PATH/psql testdb -h $SECONDARY_PGBOUNCER_HOST -p $PGBOUNCER_PORT -c "INSERT INTO student VALUES ($k);"
+  $PGBIN_PATH/psql testdb -h $SECONDARY_PGBOUNCER_HOST -p $PGBOUNCER_PORT -c "INSERT INTO teacher VALUES ($k);"
 done
 
 echo 'waiting for replication...'
 sleep 5s
 
 echo 'check replication data on MASTER...'
-$PGBIN_PATH/psql testdb -h $MASTER_HOST -p $MASTER_DB_PORT -c "SELECT max(id) FROM student"
-$PGBIN_PATH/psql testdb -h $MASTER_HOST -p $MASTER_DB_PORT -c "SELECT count('x') FROM student"
+$PGBIN_PATH/psql testdb -h $MASTER_HOST -p $MASTER_DB_PORT -c "SELECT max(id) FROM teacher"
+$PGBIN_PATH/psql testdb -h $MASTER_HOST -p $MASTER_DB_PORT -c "SELECT count('x') FROM teacher"
