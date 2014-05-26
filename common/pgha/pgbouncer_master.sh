b152d623de0a5114fa5fd511865ca4a5a2e1e7f9
sharkmao <sharkmao@silkcloud.com>
2014-05-26 10:41:45 +0800

    remove old pgha scripts

--- common/pgha/pgbouncer_master.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source set_env.sh
-source common.sh
-source pgbouncer.sh
-
-./pgbouncer.sh $MASTER_SERVER $MASTER_PORT
-
--- common/pgha/pgbouncer_slave.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source set_env.sh
-source common.sh
-source pgbouncer.sh
-
-./pgbouncer.sh $SLAVE_SERVER $SLAVE_PORT
-
