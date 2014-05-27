6e247c186cb42531791c02f5a958181a89fe915c
sharkmao <sharkmao@silkcloud.com>
2014-05-27 17:29:49 +0800

    add master/slave auth in pg_hab.conf
    Summary: add master/slave auth in pg_hba.conf
    
    Test Plan: none
    
    Reviewers: shuz, kg
    
    Subscribers: sharkmao, all
    
    Differential Revision: https://code.silkcloud.info/D144

--- scripts/pgha/setup_slave.sh
+++ scripts/pgha/setup_slave.sh
@@ -5,16 +5,13 @@ source common.sh
 checkAccount $DEPLOYMENT_ACCOUNT
 
 echo "create database data folder $SLAVE_DATA_PATH"
-mkdir -p $SLAVE_DATA_PATH
-chmod 700 $SLAVE_DATA_PATH
+createDir $SLAVE_DATA_PATH
 
 echo "create database backup folder $SLAVE_BACKUP_PATH"
-rm -rf $SLAVE_BACKUP_PATH
-mkdir $SLAVE_BACKUP_PATH
+createDir $SLAVE_BACKUP_PATH
 
 echo "create database archive folder $SLAVE_ARCHIVE_PATH"
-rm -rf $SLAVE_ARCHIVE_PATH
-mkdir $SLAVE_ARCHIVE_PATH
+createDir $SLAVE_ARCHIVE_PATH
 
 echo "copy backup file from remote master"
 rsync -azhv $DEPLOYMENT_ACCOUNT@$MASTER_HOST:$MASTER_BACKUP_PATH/* $SLAVE_DATA_PATH
@@ -26,6 +23,25 @@ restore_command = 'cp $SLAVE_ARCHIVE_PATH/%f %p'
 standby_mode = 'on'
 primary_conninfo = 'user=$PGUSER host=$MASTER_HOST port=$MASTER_DB_PORT sslmode=prefer sslcompression=1 krbsrvname=$PGUSER'
 trigger_file = '$PROMOTE_TRIGGER_FILE'
+EOF
+
+echo "configure pg_hba.conf..."
+cat >> $MASTER_DATA_PATH/pg_hba.conf <<EOF
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+
+# "local" is for Unix domain socket connections only
+local   all             ${PGUSER}                               trust
+# IPv4 local connections:
+host    all             ${PGUSER}       127.0.0.1/32            trust
+host    all             ${PGUSER}       ${MASTER_HOST}/32       trust
+host    all             ${PGUSER}       ${SLAVE_HOST}/32        trust
+host    all             ${PGUSER}       ${REPLICATION_HOST}/32  trust
+# IPv6 local connections:
+host    all             ${PGUSER}       ::1/128                 trust
+# Allow replication connections from localhost, by a user with the
+# replication privilege.
+host    replication     ${PGUSER}       ${MASTER_HOST}/32       trust
+host    replication     ${PGUSER}       ${SLAVE_HOST}/32        trust
 EOF
 
 echo "configure postgres.conf..."
