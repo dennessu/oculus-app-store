03b5c94028c7abd74671b7aa45f8032c1a20850b
yunlongzhao <yunlongzhao@wan-san.com>
2014-05-27 11:41:58 +0800

    add subledger test case and update test property

---
+++ integrationtesting/common-testlib/src/main/java/com/junbo/test/common/property/Release.java
@@ -0,0 +1,18 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (C) 2014 Junbo and/or its affiliates. All rights reserved.
+ */
+package com.junbo.test.common.property;
+
+/**
+ * Created by weiyu_000 on 5/26/14.
+ */
+public enum  Release {
+    Default,
+    July2014,
+    June2014,
+    May2014,
+    April2014,
+    March2014,
+}
