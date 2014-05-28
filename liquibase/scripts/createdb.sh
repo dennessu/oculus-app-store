efc063160b4f54531208fb50f193ad1a294a9598
Shu Zhang <shuz@silkcloud.com>
2014-05-28 06:55:11 +0000

    Change scripts to use bash. Ubuntu uses dash as default.

--- liquibase/scripts/createdb.sh
+++ liquibase/scripts/createdb.sh
@@ -1,28 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 source "$(git rev-parse --show-toplevel)/scripts/common.sh"; # this comment is needed, see common.sh for detail
 
 function createschema {
     dbname=$1
     schemaname=$2
+    host=$3
 
     echo creating schema $dbname.$schemaname
-    psqlh $dbname -c "CREATE SCHEMA IF NOT EXISTS $schemaname"
+    psqlh $dbname -c "CREATE SCHEMA IF NOT EXISTS $schemaname" -h $host
 }
 
 function createdb {
     dbname=$1
     schema=$2
+    host=$3
 
-    if psqlh_d -lt | cut -d \| -f 1 | (grep -w $dbname > /dev/null); then
+    if [[ "$host" == "" ]]; then
+        host=localhost
+    fi
+
+    if psqlh_d -lt -h $host | cut -d \| -f 1 | (grep -w $dbname > /dev/null); then
         # database exists
         echo database $dbname already exists.
     else
         # create database
         echo creating database $dbname
-        psqlh_d -c "CREATE DATABASE \"$dbname\""
+        psqlh_d -c "CREATE DATABASE \"$dbname\"" -h $host
     fi
         
-    createschema "$dbname" "$schema"
+    createschema "$dbname" "$schema" "$host"
 }
 
 createdb $*
--- liquibase/scripts/dropdb.sh
+++ liquibase/scripts/dropdb.sh
@@ -1,14 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 source "$(git rev-parse --show-toplevel)/scripts/common.sh"; # this comment is needed, see common.sh for detail
 
 function dropdb {
     dbname=$1
+    host=$2
+
+    if [[ "$host" == "" ]]; then
+        host=localhost
+    fi
 
     # for each database
-    if psqlh_d -lt | cut -d \| -f 1 | (grep -w $dbname > /dev/null); then
+    if psqlh_d -lt -h $host | cut -d \| -f 1 | (grep -w $dbname > /dev/null); then
         # database exists
         echo database $dbname exists.
-        psqlh_d -c "DROP DATABASE \"$dbname\""
+        psqlh_d -c "DROP DATABASE \"$dbname\"" -h $host
     else
         # create database
         echo database $dbname not exists
