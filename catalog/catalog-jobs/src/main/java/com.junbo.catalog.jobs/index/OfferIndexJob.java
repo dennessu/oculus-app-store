088ae716cb6c91fc991f654445610f91023959f4
baojing <baojingzuo@silkcloud.com>
2014-06-02 01:19:38 +0800

    catalog backend jobs
    Summary: catalog backend jobs
    
    Test Plan: none
    
    Reviewers: sharkmao, nanxin
    
    Subscribers: nanxin, sharkmao, all
    
    Differential Revision: https://code.silkcloud.info/D175

---
+++ catalog/catalog-jobs/src/main/java/com.junbo.catalog.jobs/index/OfferIndexJob.java
@@ -0,0 +1,22 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (C) 2014 Junbo and/or its affiliates. All rights reserved.
+ */
+
+package com.junbo.catalog.jobs.index;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * OfferIndexJob.
+ */
+public class OfferIndexJob {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OfferIndexJob.class);
+    public void execute() {
+        LOGGER.info("Start OfferIndexJob");
+        // TODO:...
+        LOGGER.info("End OfferIndex Job");
+    }
+}
