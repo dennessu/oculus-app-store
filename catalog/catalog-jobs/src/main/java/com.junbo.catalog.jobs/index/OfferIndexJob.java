16e9da8466b3d44ac893c176a06e1d0ab87cf4b6
baojing <baojingzuo@silkcloud.com>
2014-06-02 18:34:21 +0800

    backend job
    Summary: backend job
    
    Test Plan: none
    
    Reviewers: sharkmao
    
    Subscribers: sharkmao, all
    
    Differential Revision: https://code.silkcloud.info/D176

--- catalog/catalog-jobs/src/main/java/com.junbo.catalog.jobs/index/OfferIndexJob.java
+++ catalog/catalog-jobs/src/main/java/com.junbo.catalog.jobs/index/OfferIndexJob.java
@@ -6,17 +6,80 @@
 
 package com.junbo.catalog.jobs.index;
 
+import com.junbo.catalog.core.OfferService;
+import com.junbo.catalog.spec.model.offer.Offer;
+import com.junbo.catalog.spec.model.offer.OffersGetOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * OfferIndexJob.
  */
 public class OfferIndexJob {
     private static final Logger LOGGER = LoggerFactory.getLogger(OfferIndexJob.class);
+    private OfferService offerService;
+    private AtomicBoolean running = new AtomicBoolean(false);
+
+    @Required
+    public void setOfferService(OfferService offerService) {
+        this.offerService = offerService;
+    }
+
     public void execute() {
-        LOGGER.info("Start OfferIndexJob");
-        // TODO:...
-        LOGGER.info("End OfferIndex Job");
+        if (running.compareAndSet(false, true)) {
+            try {
+                LOGGER.info("Start OfferIndexJob");
+                updateActiveRevision();
+                LOGGER.info("End OfferIndex Job");
+            } finally {
+                running.set(false);
+            }
+        } else {
+            LOGGER.info("Another thread is running.");
+        }
+    }
+
+    private void updateActiveRevision() {
+        OffersGetOptions options = new OffersGetOptions();
+        options.setPublished(true);
+        List<Offer> offers = offerService.getOffers(options);
+        while (!CollectionUtils.isEmpty(offers)) {
+            Iterator<Offer> iterator = offers.iterator();
+            while (iterator.hasNext()) {
+                Offer offer = iterator.next();
+                if (offer.getCurrentRevisionId() != null && offer.getActiveRevision() != null
+                        && offer.getCurrentRevisionId().equals(offer.getActiveRevision().getRevisionId())) {
+                    continue;
+                } else if (offer.getCurrentRevisionId()==null) {
+                    LOGGER.warn("Offer " + offer.getOfferId() + " is published, but no current revision.");
+                    // TODO: TBD
+                    //offer.setPublished(false);
+                    //offerService.updateEntity(offer.getOfferId(), offer);
+                    continue;
+                }
+
+                LOGGER.info("Refresh active revision for offer: " + offer.getOfferId()
+                        + ". Active revision: " + offer.getCurrentRevisionId());
+                try {
+                    offer.setActiveRevision(offerService.getRevision(offer.getCurrentRevisionId()));
+                    offerService.updateEntity(offer.getOfferId(), offer);
+                    for (int i=0; i<10000; i++) {
+                        System.out.println(i*10+2);
+                    }
+                } catch (Exception e) {
+                    LOGGER.error("Error updating offer " + offer.getOfferId(), e);
+                }
+
+            }
+
+            options.nextPage();
+            offers = offerService.getOffers(options);
+        }
     }
 }
