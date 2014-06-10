6b8cbfce256af3db56153ecb96adeb2f1fbe88e6
zhany <zhany@silkcloud.com>
2014-06-10 22:09:43 +0800

    Add accessToken and refreshToken resources
    Test Plan: build
    
    Reviewers: kg
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D237

---
+++ oauth/oauth-api/src/main/groovy/com/junbo/oauth/api/endpoint/AccessTokenResourceImpl.groovy
@@ -0,0 +1,47 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (C) 2014 Junbo and/or its affiliates. All rights reserved.
+ */
+package com.junbo.oauth.api.endpoint
+
+import com.junbo.authorization.AuthorizeContext
+import com.junbo.langur.core.promise.Promise
+import com.junbo.oauth.core.exception.AppExceptions
+import com.junbo.oauth.core.service.TokenService
+import com.junbo.oauth.spec.endpoint.AccessTokenResource
+import com.junbo.oauth.spec.model.AccessToken
+import groovy.transform.CompileStatic
+import org.springframework.beans.factory.annotation.Required
+import org.springframework.context.annotation.Scope
+import org.springframework.stereotype.Component
+
+/**
+ * AccessTokenResourceImpl.
+ */
+@Component
+@CompileStatic
+@Scope('prototype')
+class AccessTokenResourceImpl implements AccessTokenResource {
+    private static final String TOKEN_INFO_SCOPE = 'token.info'
+    private TokenService tokenService
+
+    @Required
+    void setTokenService(TokenService tokenService) {
+        this.tokenService = tokenService
+    }
+
+    Promise<AccessToken> get(String accessToken) {
+        if (!AuthorizeContext.hasScopes(TOKEN_INFO_SCOPE)) {
+            throw AppExceptions.INSTANCE.insufficientScope().exception()
+        }
+
+        AccessToken token = tokenService.getAccessToken(accessToken)
+
+        if (token == null) {
+            throw AppExceptions.INSTANCE.invalidAccessToken(accessToken).exception()
+        }
+
+        return Promise.pure(token)
+    }
+}
