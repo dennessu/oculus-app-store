15f055a9d4ab38aa7043047ea7cd89e4ef44c237
zhany <zhany@silkcloud.com>
2014-06-16 16:27:23 +0800

    Add authorization support for fulfilment component
    Test Plan: build
    
    Reviewers: kg, sharkmao
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D272

--- fulfilment/fulfilment-rest/src/main/java/com/junbo/fulfilment/rest/resource/FulfilmentResourceImpl.java
+++ fulfilment/fulfilment-rest/src/main/java/com/junbo/fulfilment/rest/resource/FulfilmentResourceImpl.java
@@ -5,6 +5,8 @@
  */
 package com.junbo.fulfilment.rest.resource;
 
+import com.junbo.authorization.AuthorizeContext;
+import com.junbo.authorization.spec.error.AppErrors;
 import com.junbo.common.id.FulfilmentId;
 import com.junbo.common.id.OrderId;
 import com.junbo.fulfilment.core.FulfilmentService;
@@ -25,22 +27,35 @@ import javax.ws.rs.core.MediaType;
 @Produces({MediaType.APPLICATION_JSON})
 @Consumes({MediaType.APPLICATION_JSON})
 public class FulfilmentResourceImpl implements FulfilmentResource {
+    private static final String FULFILMENT_SERVICE_SCOPE = "fulfilment.service";
+    private static final String FULFILMENT_CSR_SCOPE = "fulfilment.csr";
+
     @Autowired
     private FulfilmentService service;
 
     @POST
     public Promise<FulfilmentRequest> fulfill(@Valid FulfilmentRequest request) {
+        authorize();
         FulfilmentRequest result = service.fulfill(request);
         return Promise.pure(result);
     }
 
     @GET
     public Promise<FulfilmentRequest> getByOrderId(@QueryParam("orderId") OrderId orderId) {
+        authorize();
         return Promise.pure(service.retrieveRequestByOrderId(orderId.getValue()));
     }
 
     @Override
     public Promise<FulfilmentItem> getByFulfilmentId(@PathParam("fulfilmentId") FulfilmentId fulfilmentId) {
+        authorize();
         return Promise.pure(service.retrieveFulfilmentItem(fulfilmentId.getValue()));
+    }
+
+    private static void authorize() {
+        if (!AuthorizeContext.hasScopes(FULFILMENT_SERVICE_SCOPE)
+                && !AuthorizeContext.hasScopes(FULFILMENT_CSR_SCOPE)) {
+            throw AppErrors.INSTANCE.insufficientScope().exception();
+        }
     }
 }
