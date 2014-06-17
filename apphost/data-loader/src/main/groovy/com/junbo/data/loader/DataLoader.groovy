ea64a46f53bda164d677b1d4303cf0c81c4afb18
nanxin <nanxin@silkcloud.com>
2014-06-17 13:04:36 +0800

    dataloader
    Summary: fix encoding problem;
    
    Test Plan: fullcycle
    
    Reviewers: shuz
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D301

--- apphost/data-loader/src/main/groovy/com/junbo/data/loader/DataLoader.groovy
+++ apphost/data-loader/src/main/groovy/com/junbo/data/loader/DataLoader.groovy
@@ -25,11 +25,9 @@ class DataLoader {
     static PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(this.class.classLoader)
 
     static void main(String[] args) {
-        if (args.length == 0) {
-            printUsage()
-        }
         new ClassPathXmlApplicationContext("classpath*:/spring/*-context.xml")
-        if (args[0].equalsIgnoreCase("all")) {
+
+        if (args.length == 0 || args[0].equalsIgnoreCase("all")) {
             try {
                 load(dataList)
             } finally {
@@ -37,38 +35,43 @@ class DataLoader {
             }
         } else {
             List<String> list = CollectionUtils.arrayToList(args)
-            list.each { data ->
-                if (!dataList.contains(data)) {
-                    System.out.println("$data not found")
-                    exit()
-                }
-            }
+
+            checkData(list)
+
             try {
                 load(list)
-            } catch (Exception e){
-                System.out.println(e.getStackTrace())
+            } finally {
                 exit()
             }
-            exit()
         }
     }
 
-    static void printUsage() {
-        System.out.println('input data to load and input all to load all data')
-        exit()
+    static void checkData(List<String> list) {
+        list.each { data ->
+            if (!dataList.contains(data)) {
+                System.out.println("$data not found")
+                exit()
+            }
+        }
     }
 
     static void load(List<String> dataList) {
         for (String data : dataList) {
-            Resource[] resources = resolver.getResources("data/$data/*.data")
-            for (Resource resource : resources) {
-                DataHandler handler = handlers[data]
-                if (handler != null) {
-                    String content = IOUtils.toString(resource.URI)
-                    handler.handle(content)
-                } else {
-                    System.out.println("no handler for $data")
+            try {
+                Resource[] resources = resolver.getResources("data/$data/*.data")
+                for (Resource resource : resources) {
+                    DataHandler handler = handlers[data]
+                    if (handler != null) {
+                        String content = IOUtils.toString(resource.URI)
+                        handler.handle(content)
+                    } else {
+                        System.out.println("no handler for $data")
+                    }
                 }
+            } catch (Exception e) {
+                System.out.println("Error ocuured while loading $data")
+                System.out.println(e.getStackTrace())
+                exit()
             }
         }
     }
