9cb09c58bc9d2a3bb11e7c7af266cb972af8ab53
shuz <shuz@silkcloud.com>
2014-06-18 15:28:09 +0800

    Fix the cloudant serialization, don't ignore id/self
    Summary:
    Fix the cloudant serialization.
    Deleted old cloudant script.
    Add postman proxy to help using postman.
    
    Test Plan: fullcycle
    
    Reviewers: zhany
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D319

--- scripts/dropall-cloudant.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ -z $1 ]]; then
-    echo Usage: dropall-cloudant.sh \<prefix\>
-    exit 1
-fi
-python ./couchdbcmd.py dropall 'https://silkcloudtest1:#Bugsfor$@silkcloudtest1.cloudant.com' $1
