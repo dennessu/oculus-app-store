893e371845e1a3523e4b5fbb4abd9c7281291064
minhao <haomin@silkcloud.com>
2014-06-24 13:29:44 +0800

    enable ewallet cloudant repository

--- ewallet/ewallet-db/src/main/groovy/com/junbo/ewallet/db/repo/cloudant/LotTransactionRepositoryCloudantImpl.groovy
+++ ewallet/ewallet-db/src/main/groovy/com/junbo/ewallet/db/repo/cloudant/LotTransactionRepositoryCloudantImpl.groovy
@@ -4,13 +4,21 @@ import com.junbo.ewallet.db.repo.LotTransactionRepository
 import com.junbo.ewallet.spec.model.LotTransaction
 import com.junbo.langur.core.promise.Promise
 import com.junbo.sharding.repo.BaseCloudantRepositoryForDualWrite
+import groovy.transform.CompileStatic
 
 /**
  * Created by minhao on 6/23/14.
  */
+@CompileStatic
 class LotTransactionRepositoryCloudantImpl extends BaseCloudantRepositoryForDualWrite<LotTransaction, Long> implements LotTransactionRepository {
     @Override
     Promise<List<LotTransaction>> getByTransactionId(Long transactionId) {
-        return null
+        return super.queryView('by_transaction_id', transactionId.toString()).then { List<LotTransaction> list ->
+            list.removeAll { LotTransaction lotTransaction ->
+                return lotTransaction.unrefundedAmount == 0
+            }
+
+            return Promise.pure(list);
+        }
     }
 }
--- ewallet/ewallet-db/src/main/groovy/com/junbo/ewallet/db/repo/cloudant/WalletRepositoryCloudantImpl.groovy
+++ ewallet/ewallet-db/src/main/groovy/com/junbo/ewallet/db/repo/cloudant/WalletRepositoryCloudantImpl.groovy
@@ -5,23 +5,43 @@ import com.junbo.ewallet.spec.def.WalletType
 import com.junbo.ewallet.spec.model.Wallet
 import com.junbo.langur.core.promise.Promise
 import com.junbo.sharding.repo.BaseCloudantRepositoryForDualWrite
+import groovy.transform.CompileStatic
 
 /**
  * Created by minhao on 6/23/14.
  */
+@CompileStatic
 class WalletRepositoryCloudantImpl extends BaseCloudantRepositoryForDualWrite<Wallet, Long> implements WalletRepository {
     @Override
     Promise<Wallet> getByTrackingUuid(Long shardMasterId, UUID uuid) {
-        return null
+        return super.queryView('by_tracking_uuid', uuid.toString()).then { List<Wallet> list ->
+            if (list.size() > 0) {
+                return Promise.pure(list.get(0))
+            }
+            else {
+                return Promise.pure(null)
+            }
+        }
     }
 
     @Override
     Promise<Wallet> get(Long userId, WalletType type, String currency) {
-        return null
+        return super.queryView('by_user_id', userId.toString()).then { List<Wallet> list ->
+            list.retainAll { Wallet wallet ->
+                return wallet.type == type.toString() && wallet.currency == currency
+            }
+
+            if (list.size() > 0) {
+                return Promise.pure(list.get(0))
+            }
+            else {
+                return Promise.pure(null)
+            }
+        }
     }
 
     @Override
     Promise<List<Wallet>> getAll(Long userId) {
-        return null
+        return super.queryView('by_user_id', userId.toString())
     }
 }
