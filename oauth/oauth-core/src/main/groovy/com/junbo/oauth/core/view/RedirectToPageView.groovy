14eab39dd4582f3027c040019570a4e2c23d2042
minhao <haomin@silkcloud.com>
2014-07-02 13:57:09 +0800

    1.remove redirect view  2.add shortcut from register view to login view

--- oauth/oauth-core/src/main/groovy/com/junbo/oauth/core/view/RedirectToPageView.groovy
+++ oauth/oauth-core/src/main/groovy/com/junbo/oauth/core/view/RedirectToPageView.groovy
@@ -14,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder
  * Created by minhao on 5/21/14.
  */
 @CompileStatic
+@Deprecated
 class RedirectToPageView extends AbstractView {
 
     private String pageUrl
