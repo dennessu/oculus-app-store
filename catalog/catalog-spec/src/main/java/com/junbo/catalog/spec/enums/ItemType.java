f031ef9b989f15883efbbe5d6cf0b9745f22015a
baojing <baojingzuo@silkcloud.com>
2014-07-04 16:56:18 +0800

    remove DIGITAL & VIRTUAL

--- catalog/catalog-spec/src/main/java/com/junbo/catalog/spec/enums/ItemType.java
+++ catalog/catalog-spec/src/main/java/com/junbo/catalog/spec/enums/ItemType.java
@@ -13,10 +13,6 @@ import java.util.List;
  * Item type.
  */
 public enum ItemType {
-    // to be deleted: DIGITAL & VIRTUAL
-    // DIGITAL --> APP & DOWNLOADED_ADDITION
-    // VIRTUAL --> IN_APP_LOCK & IN_APP_CONSUMABLE
-    DIGITAL, VIRTUAL,
     PHYSICAL, APP, DOWNLOADED_ADDITION, STORED_VALUE, SUBSCRIPTION, IN_APP_UNLOCK, IN_APP_CONSUMABLE;
 
     public static final List<ItemType> ALL = Arrays.asList(ItemType.values());
