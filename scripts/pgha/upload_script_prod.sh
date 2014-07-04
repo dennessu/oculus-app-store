3cb1f4c66e9e780a8c6d9dd3b697ae0a35dadb01
sharkmao <sharkmao@silkcloud.com>
2014-07-04 14:51:39 +0800

    add prod upload script

--- scripts/pgha/upload_script_prod.sh
+++ scripts/pgha/upload_script_prod.sh
@@ -4,43 +4,43 @@ source ${DIR}/util/common.sh
 
 function uploadMaster {
 ssh -o "StrictHostKeyChecking no" $DEPLOYMENT_ACCOUNT@$MASTER_HOST << ENDSSH
-sudo rm -rf $DEPLOYMENT_PATH
+rm -rf $DEPLOYMENT_PATH
 mkdir $DEPLOYMENT_PATH
 ENDSSH
 
 scp -r ./ $DEPLOYMENT_ACCOUNT@$MASTER_HOST:$DEPLOYMENT_PATH
 
 ssh -o "StrictHostKeyChecking no" $DEPLOYMENT_ACCOUNT@$MASTER_HOST << ENDSSH
-sudo chown $DEPLOYMENT_ACCOUNT:$DEPLOYMENT_ACCOUNT $DEPLOYMENT_PATH/*
+chown $DEPLOYMENT_ACCOUNT:$DEPLOYMENT_ACCOUNT $DEPLOYMENT_PATH/*
 ENDSSH
 }
 
 function uploadSlave {
 ssh -o "StrictHostKeyChecking no" $DEPLOYMENT_ACCOUNT@$SLAVE_HOST << ENDSSH
-sudo rm -rf $DEPLOYMENT_PATH
+rm -rf $DEPLOYMENT_PATH
 mkdir $DEPLOYMENT_PATH
 ENDSSH
 
 scp -r ./ $DEPLOYMENT_ACCOUNT@$SLAVE_HOST:$DEPLOYMENT_PATH
 
 ssh -o "StrictHostKeyChecking no" $DEPLOYMENT_ACCOUNT@$SLAVE_HOST << ENDSSH
-sudo chown $DEPLOYMENT_ACCOUNT:$DEPLOYMENT_ACCOUNT $DEPLOYMENT_PATH/*
+chown $DEPLOYMENT_ACCOUNT:$DEPLOYMENT_ACCOUNT $DEPLOYMENT_PATH/*
 ENDSSH
 }
 
 function uploadReplica {
 ssh -o "StrictHostKeyChecking no" $DEPLOYMENT_ACCOUNT@$REPLICA_HOST << ENDSSH
-sudo rm -rf $DEPLOYMENT_PATH
+rm -rf $DEPLOYMENT_PATH
 mkdir $DEPLOYMENT_PATH
 ENDSSH
 
 scp -r ./ $DEPLOYMENT_ACCOUNT@$REPLICA_HOST:$DEPLOYMENT_PATH
 
 ssh -o "StrictHostKeyChecking no" $DEPLOYMENT_ACCOUNT@$REPLICA_HOST << ENDSSH
-sudo chown $DEPLOYMENT_ACCOUNT:$DEPLOYMENT_ACCOUNT $DEPLOYMENT_PATH/*
+chown $DEPLOYMENT_ACCOUNT:$DEPLOYMENT_ACCOUNT $DEPLOYMENT_PATH/*
 ENDSSH
 }
 
-uploadMaster && echo "upload master done!" &
-uploadSlave && echo "upload slave done!" &
-uploadReplica && echo "upload replica done!" &
+uploadMaster && echo "upload master done!"
+uploadSlave && echo "upload slave done!"
+uploadReplica && echo "upload replica done!"
