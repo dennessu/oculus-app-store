1ebfc2e8c666e5b3e000ebe59c35cf258b3b3f6b
sharkmao <sharkmao@silkcloud.com>
2014-07-10 12:23:59 +0800

    update pgha scripts

--- scripts/pgha/env/ppe_0.sh
+++ scripts/pgha/env/ppe_0.sh
@@ -2,8 +2,8 @@
 
 #https://oculus.atlassian.net/wiki/pages/viewpage.action?spaceKey=SER&title=AWS-test+servers
 
-export MASTER_HOST=10.32.16.50
-export SLAVE_HOST=10.32.20.50
-export REPLICA_HOST=10.32.22.50
+export MASTER_HOST=10.24.16.50
+export SLAVE_HOST=10.24.20.50
+export REPLICA_HOST=10.24.22.50
 
 export REPLICA_DATABASES=('postgres' 'billing' 'ewallet' 'fulfilment' 'order' 'payment' 'sharding' 'subscription')
