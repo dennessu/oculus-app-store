6697b24ffdb6009f094c1508981c3647449cfb07
Shu Zhang <shuz@silkcloud.com>
2014-07-18 06:48:14 +0000

    Fix PPE scripts

--- scripts/utils/ppe/foreach-here.sh
+++ scripts/utils/ppe/foreach-here.sh
@@ -3,7 +3,7 @@ ssh_script=`cat`
 for p in `cat $1`; do
 	if [[ ! -z "$p" && ! $p == \#* ]]; then
 		echo running on $p
-		sudo -u silkcloud ssh -n $p "$ssh_script"
+		ssh -n $p "$ssh_script"
 	fi
 done
 
--- scripts/utils/ppe/refresh-ppe-dbs.sh
+++ scripts/utils/ppe/refresh-ppe-dbs.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "$0" )" && pwd )"
+set -e 
+
 pushd $DIR
 
 echo -n CRYPTO_KEY: 
 read -s CRYPTO_KEY
 
 YOUR_USER=shuz
+echo Copying files...
 cp /home/$YOUR_USER/main/apphost/apphost-identity/build/distributions/apphost-identity-0.0.1-SNAPSHOT.zip /home/silkcloud
 cp /home/$YOUR_USER/main/apphost/apphost-crypto/build/distributions/apphost-crypto-0.0.1-SNAPSHOT.zip /home/silkcloud
 cp /home/$YOUR_USER/main/apphost/apphost-dataloader/build/distributions/apphost-dataloader-0.0.1-SNAPSHOT.zip /home/silkcloud
 
+function pause() {
+#read -p "Press any key to continue..."
+echo pause
+}
 
-scp /home/silkcloud/apphost-identity-0.0.1-SNAPSHOT.zip silkcloud@10.24.34.10:/home/silkcloud
-ssh 10.24.34.10 << EOF
+cd /home/silkcloud
 unzip -o apphost-identity-0.0.1-SNAPSHOT.zip
 cd apphost-identity-0.0.1-SNAPSHOT/dbsetup/pgha
 ./upload_script.sh ppe
-EOF
+cd $DIR
 
 ./foreach-here.sh ppe-masters.txt << EOF
 /var/silkcloud/pgha/purge/purge_master.sh
 EOF
+pause
 
 ssh 10.24.20.50 << EOF
 /var/silkcloud/pgha/purge/purge_slave.sh
 EOF
+pause
 
 ssh 10.24.22.50 << EOF
 /var/silkcloud/pgha/purge/purge_replica.sh
 EOF
-
+pause
 
 ./foreach-here.sh ppe-masters.txt << EOF
 cd /var/silkcloud/pgha
