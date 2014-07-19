2ce4615cfcfcd8602dbf888f9c1fc958535264e6
nanxin <nanxin@silkcloud.com>
2014-07-19 18:34:28 +0800

    refine dataloader
    Summary: refine dataloader
    
    Test Plan: UT
    
    Reviewers: jasonfu
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D570

--- apphost/data-loader/src/main/groovy/com/junbo/data/handler/CatalogDataHandler.groovy
+++ apphost/data-loader/src/main/groovy/com/junbo/data/handler/CatalogDataHandler.groovy
@@ -9,12 +9,12 @@ import com.junbo.catalog.spec.model.item.Item
 import com.junbo.catalog.spec.model.item.ItemRevision
 import com.junbo.catalog.spec.model.offer.Offer
 import com.junbo.catalog.spec.model.offer.OfferRevision
+import com.junbo.catalog.spec.resource.ItemResource
+import com.junbo.catalog.spec.resource.ItemRevisionResource
+import com.junbo.catalog.spec.resource.OfferResource
+import com.junbo.catalog.spec.resource.OfferRevisionResource
 import com.junbo.common.id.OrganizationId
 import com.junbo.common.model.Results
-import com.junbo.data.handler.catalog.ItemDataHandler
-import com.junbo.data.handler.catalog.ItemRevisionDataHandler
-import com.junbo.data.handler.catalog.OfferDataHandler
-import com.junbo.data.handler.catalog.OfferRevisionDataHandler
 import com.junbo.data.model.CatalogData
 import com.junbo.identity.spec.v1.model.Organization
 import com.junbo.identity.spec.v1.option.list.OrganizationListOptions
@@ -28,10 +28,10 @@ import org.springframework.core.io.Resource
  */
 class CatalogDataHandler extends BaseDataHandler {
     private OrganizationResource organizationResource
-    private ItemDataHandler itemDataHandler
-    private ItemRevisionDataHandler itemRevisionDataHandler
-    private OfferDataHandler offerDataHandler
-    private OfferRevisionDataHandler offerRevisionDataHandler
+    private ItemResource itemResource
+    private ItemRevisionResource itemRevisionResource
+    private OfferResource offerResource
+    private OfferRevisionResource offerRevisionResource
 
     @Required
     void setOrganizationResource(OrganizationResource organizationResource) {
@@ -39,23 +39,23 @@ class CatalogDataHandler extends BaseDataHandler {
     }
 
     @Required
-    void setItemDataHandler(ItemDataHandler itemDataHandler) {
-        this.itemDataHandler = itemDataHandler
+    void setItemResource(ItemResource itemResource) {
+        this.itemResource = itemResource
     }
 
     @Required
-    void setItemRevisionDataHandler(ItemRevisionDataHandler itemRevisionDataHandler) {
-        this.itemRevisionDataHandler = itemRevisionDataHandler
+    void setItemRevisionResource(ItemRevisionResource itemRevisionResource) {
+        this.itemRevisionResource = itemRevisionResource
     }
 
     @Required
-    void setOfferDataHandler(OfferDataHandler offerDataHandler) {
-        this.offerDataHandler = offerDataHandler
+    void setOfferResource(OfferResource offerResource) {
+        this.offerResource = offerResource
     }
 
     @Required
-    void setOfferRevisionDataHandler(OfferRevisionDataHandler offerRevisionDataHandler) {
-        this.offerRevisionDataHandler = offerRevisionDataHandler
+    void setOfferRevisionResource(OfferRevisionResource offerRevisionResource) {
+        this.offerRevisionResource = offerRevisionResource
     }
 
     @Override
@@ -65,7 +65,7 @@ class CatalogDataHandler extends BaseDataHandler {
 
     @Override
     void handle(String content) {
-        CatalogData catalogData
+        CatalogData catalogData = null
         try {
             catalogData = transcoder.decode(new TypeReference<CatalogData>() {}, content) as CatalogData
         } catch (Exception e) {
@@ -79,37 +79,41 @@ class CatalogDataHandler extends BaseDataHandler {
             exit()
         }
 
+        logger.info("----loading item")
         Item item = catalogData.item
         item.id = null
         item.ownerId = ownerId
-        String itemId = itemDataHandler.handle(item)
+        String itemId = handle(item)
 
+        logger.info("----loading itemRevision")
         ItemRevision itemRevision = catalogData.itemRevision
         itemRevision.id = null
         itemRevision.ownerId = ownerId
         itemRevision.itemId = itemId
-        itemRevisionDataHandler.handle(itemRevision)
+        handle(itemRevision)
 
+        logger.info("----loading offer")
         Offer offer = catalogData.offer
         offer.id = null
         offer.ownerId = ownerId
-        String offerId = offerDataHandler.handle(offer)
+        String offerId = handle(offer)
 
+        logger.info("----loading offerRevision")
         OfferRevision offerRevision = catalogData.offerRevision
         offerRevision.id = null
         offerRevision.ownerId = ownerId
         offerRevision.items.get(0).itemId = itemId
         offerRevision.offerId = offerId
-        offerRevisionDataHandler.handle(offerRevision)
+        handle(offerRevision)
+
     }
 
-    private OrganizationId getOwner(String organizationName) {
+    OrganizationId getOwner(String organizationName) {
         logger.debug('Get organization info')
         Organization organization
 
         //Get organization info
         try {
-
             Results<Organization> results = organizationResource.list(new OrganizationListOptions(name: organizationName)).get()
             if (results != null && results.items != null && results.items.size() > 0) {
                 organization = results.items.get(0)
@@ -121,4 +125,57 @@ class CatalogDataHandler extends BaseDataHandler {
 
         return organization.id as OrganizationId
     }
+
+    String handle(Item item) {
+        logger.debug('Create new item with this content')
+        try {
+            Item result = itemResource.create(item).get()
+            return result.id
+        } catch (Exception e) {
+            logger.error("Error creating item $item.itemId.", e)
+            return null
+        }
+    }
+
+
+    void handle(ItemRevision itemRevision) {
+        logger.debug('Create new item revision with this content')
+        try {
+            ItemRevision itemRevisionCreated = itemRevisionResource.createItemRevision(itemRevision).get()
+
+            logger.debug('put the item revision to APPROVED')
+            itemRevisionCreated.setStatus("APPROVED")
+            itemRevisionResource.updateItemRevision(itemRevisionCreated.revisionId, itemRevisionCreated).get()
+        } catch (Exception e) {
+            logger.error("Error creating item revision $itemRevision.revisionId.", e)
+        }
+    }
+
+
+    String handle(Offer offer) {
+        logger.debug('Create new offer with this content')
+        try {
+            Offer result = offerResource.create(offer).get()
+            return result.id
+        } catch (Exception e) {
+            logger.error("Error creating offer $offer.offerId.", e)
+            return null
+        }
+
+    }
+
+
+    void handle(OfferRevision offerRevision) {
+        logger.debug('Create new offer revision with this content')
+        try {
+            OfferRevision offerRevisionCreated = offerRevisionResource.createOfferRevision(offerRevision).get()
+
+            logger.debug('put the offer revision to APPROVED')
+            offerRevisionCreated.setStatus("APPROVED")
+            offerRevisionResource.updateOfferRevision(offerRevisionCreated.revisionId, offerRevisionCreated).get()
+        } catch (Exception e) {
+            logger.error("Error creating offer revision $offerRevision.revisionId.", e)
+        }
+    }
+
 }
