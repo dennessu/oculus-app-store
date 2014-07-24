3ad866f3f7b5b03b1b3cee36ad2e079092a33694
txchen <txchen@silkcloud.com>
2014-07-23 23:21:27 -0700

    remove old docker files

--- devenv/docker-images/onebox-couchdb/runcouchdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mkdir -p /var/run/couchdb
-chown couchdb:couchdb /var/run/couchdb
-
-chown -R couchdb /var/lib/couchdb
-chown -R couchdb /var/log/couchdb
-
-exec /sbin/setuser couchdb /usr/bin/couchdb
