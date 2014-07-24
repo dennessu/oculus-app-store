6d136b4ea2465480d39d5c13df810a1f9d350fdf
yilin <yilin@silkcloud.com>
2014-07-24 16:06:55 +0800

    add payment url back in put order
    Summary: add payment url back in put order
    
    Test Plan: none
    
    Reviewers: cz, xmchen, fzhang
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D631

--- order/order-core/src/main/groovy/com/junbo/order/core/impl/order/OrderServiceImpl.groovy
+++ order/order-core/src/main/groovy/com/junbo/order/core/impl/order/OrderServiceImpl.groovy
@@ -83,6 +83,8 @@ class OrderServiceImpl implements OrderService {
             }
             // TODO: compare the reqeust and the order persisted
             orderValidator.validateSettleOrderRequest(ratedOrder)
+            ratedOrder.payments[0].successRedirectUrl = order.payments[0].successRedirectUrl
+            ratedOrder.payments[0].cancelRedirectUrl = order.payments[0].cancelRedirectUrl
             ratedOrder.purchaseTime = ratedOrder.honoredTime
 
             Throwable error
