279cd922adeb440fa32c6990ab54f83fab728004
zhany <zhany@silkcloud.com>
2014-07-24 14:08:35 +0800

    Add authorization for migration api
    Test Plan: build
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D630

--- identity/identity-rest/src/main/groovy/com/junbo/identity/rest/resource/v1/MigrationResourceImpl.groovy
+++ identity/identity-rest/src/main/groovy/com/junbo/identity/rest/resource/v1/MigrationResourceImpl.groovy
@@ -1,4 +1,6 @@
 package com.junbo.identity.rest.resource.v1
+
+import com.junbo.authorization.AuthorizeContext
 import com.junbo.authorization.spec.model.Role
 import com.junbo.authorization.spec.model.RoleAssignment
 import com.junbo.authorization.spec.model.RoleTarget
@@ -98,6 +100,10 @@ class MigrationResourceImpl implements MigrationResource {
     @Override
     @Transactional
     Promise<OculusOutput> migrate(OculusInput oculusInput) {
+        if (!AuthorizeContext.hasScopes('identity.admin')) {
+            throw AppCommonErrors.INSTANCE.forbidden().exception()
+        }
+
         if (StringUtils.isEmpty(oculusInput.username)) {
             throw new IllegalArgumentException('username can\'t be null')
         }
@@ -129,6 +135,10 @@ class MigrationResourceImpl implements MigrationResource {
     @Override
     @Transactional
     Promise<Map<String, OculusOutput>> bulkMigrate(List<OculusInput> oculusInputs) {
+        if (!AuthorizeContext.hasScopes('identity.admin')) {
+            throw AppCommonErrors.INSTANCE.forbidden().exception()
+        }
+
         CloudantClientBase.useBulk = true
         CloudantClientBulk.callback = new MigrationQueryViewCallback()
 
