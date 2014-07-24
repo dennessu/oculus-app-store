f83160154831139a09914015ea37a2c8e23566f5
txchen <txchen@silkcloud.com>
2014-07-23 20:44:52 -0700

    fix onebox-couchdb image

--- devenv/docker-images/onebox-couchdb/runcouchdb.sh
+++ devenv/docker-images/onebox-couchdb/runcouchdb.sh
@@ -2,4 +2,7 @@
 mkdir -p /var/run/couchdb
 chown couchdb:couchdb /var/run/couchdb
 
+chown -R couchdb /var/lib/couchdb
+chown -R couchdb /var/log/couchdb
+
 exec /sbin/setuser couchdb /usr/bin/couchdb
