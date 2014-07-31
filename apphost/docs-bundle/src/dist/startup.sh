3d67a40e7537e555b460274183132f354957757b
txchen <txchen@silkcloud.com>
2014-07-30 21:20:30 -0700

    make docs bundle start.sh in fg
    Summary: make docs bundle start.sh in fg
    
    Test Plan: manual
    
    Reviewers: shuz
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D728

--- apphost/docs-bundle/src/dist/startup.sh
+++ apphost/docs-bundle/src/dist/startup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 (set -o igncr) 2>/dev/null && set -o igncr; # ignore \r in windows. The comment is needed.
-DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-./bin/docs-bundle &
+exec ./bin/docs-bundle
