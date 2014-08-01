5850c8983eea731b9f52b0ea2cb8800b036fee86
JasonFu <jasonfu@silkcloud.com>
2014-08-01 18:10:46 +0800

    update put item revision
    Summary: update test cases for putting item revision
    
    Test Plan: build
    
    Reviewers: jiefeng
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D762

--- integrationtesting/catalog-integration/src/test/java/com/junbo/test/catalog/itemRevision/TestPutItemRevision.java
+++ integrationtesting/catalog-integration/src/test/java/com/junbo/test/catalog/itemRevision/TestPutItemRevision.java
@@ -5,6 +5,13 @@
  */
 package com.junbo.test.catalog.itemRevision;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.junbo.catalog.spec.model.item.Binary;
+import com.junbo.catalog.spec.model.item.ItemRevisionLocaleProperties;
+import com.junbo.catalog.spec.model.offer.ItemEntry;
+import com.junbo.catalog.spec.model.offer.OfferRevision;
+import com.junbo.identity.spec.v1.model.Organization;
 import com.junbo.test.common.apihelper.identity.impl.OrganizationServiceImpl;
 import com.junbo.test.common.apihelper.identity.OrganizationService;
 import com.junbo.test.catalog.impl.ItemRevisionServiceImpl;
@@ -15,16 +22,22 @@ import com.junbo.test.catalog.impl.ItemServiceImpl;
 import com.junbo.test.catalog.ItemRevisionService;
 import com.junbo.test.catalog.util.BaseTestClass;
 import com.junbo.catalog.spec.model.item.Item;
+import com.junbo.test.common.blueprint.Master;
+import com.junbo.test.common.libs.IdConverter;
 import com.junbo.test.common.libs.LogHelper;
 import com.junbo.common.id.OrganizationId;
 import com.junbo.test.catalog.ItemService;
+import com.junbo.test.common.libs.RandomFactory;
 import com.junbo.test.common.property.*;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Jason
@@ -37,6 +50,7 @@ public class TestPutItemRevision extends BaseTestClass {
 
     private Item item1;
     private Item item2;
+    private Organization organization;
     private OrganizationId organizationId;
     private final String defaultLocale = "en_US";
     private final String fullItemRevisionFileName = "fullItemRevision";
@@ -47,7 +61,8 @@ public class TestPutItemRevision extends BaseTestClass {
     @BeforeClass
     private void PrepareTestData() throws Exception {
         OrganizationService organizationService = OrganizationServiceImpl.instance();
-        organizationId = organizationService.postDefaultOrganization().getId();
+        organization = organizationService.postDefaultOrganization();
+        organizationId = organization.getId();
 
         item1 = itemService.postDefaultItem(CatalogItemType.getRandom(), organizationId);
         item2 = itemService.postDefaultItem(CatalogItemType.getRandom(), organizationId);
@@ -68,7 +83,11 @@ public class TestPutItemRevision extends BaseTestClass {
     )
     @Test
     public void testPutItemRevision() throws Exception {
+        OrganizationService organizationService = OrganizationServiceImpl.instance();
+        Organization organizationTmp = organizationService.postDefaultOrganization();
+        OrganizationId organizationIdTmp = organizationTmp.getId();
 
+        Master.getInstance().setCurrentUid(IdConverter.idToHexString(organization.getOwnerId()));
         ItemRevision itemRevision = itemRevisionService.postDefaultItemRevision(item1);
 
         //update status
@@ -79,7 +98,173 @@ public class TestPutItemRevision extends BaseTestClass {
         itemRevision = itemRevisionService.updateItemRevision(itemRevision.getRevisionId(), itemRevision);
 
         itemRevision.setStatus(CatalogEntityStatus.APPROVED.name());
+        itemRevisionService.updateItemRevision(itemRevision.getRevisionId(), itemRevision);
+
+        //update item field
+        item1 = itemService.getItem(item1.getItemId());
+
+        itemRevision = itemRevisionService.postDefaultItemRevision(item1);
+        itemRevision.setItemId(item2.getItemId());
         itemRevision = itemRevisionService.updateItemRevision(itemRevision.getRevisionId(), itemRevision);
+
+        //update developer field
+        Master.getInstance().setCurrentUid(IdConverter.idToHexString(organizationTmp.getOwnerId()));
+        item2.setOwnerId(organizationIdTmp);
+        itemService.updateItem(item2.getItemId(), item2);
+
+        itemRevision.setOwnerId(organizationIdTmp);
+        itemRevision = itemRevisionService.updateItemRevision(itemRevision.getRevisionId(), itemRevision);
+
+        //update platforms
+        List<String> platforms = new ArrayList<>();
+        platforms.add("PC");
+        platforms.add("MAC");
+        platforms.add("LINUX");
+        platforms.add("ANDROID");
+        itemRevision.setPlatforms(platforms);
+        itemRevision = itemRevisionService.updateItemRevision(itemRevision.getRevisionId(), itemRevision);
+
+        //update user interaction Modes
+        List<String> interactionModes = new ArrayList<>();
+        interactionModes.add("SINGLE_USER");
+        interactionModes.add("MULTI_USER");
+        interactionModes.add("CO_OP");
+        itemRevision.setUserInteractionModes(interactionModes);
+        itemRevision = itemRevisionService.updateItemRevision(itemRevision.getRevisionId(), itemRevision);
+
+        //update package name
+        itemRevision.setPackageName(RandomFactory.getRandomStringOfAlphabet(20));
+        itemRevision = itemRevisionService.updateItemRevision(itemRevision.getRevisionId(), itemRevision);
+
+        //update locales
+        Map<String, ItemRevisionLocaleProperties> locales = itemRevision.getLocales();
+        ItemRevisionLocaleProperties itemRevisionLocaleProperties = locales.get(defaultLocale);
+
+        if (itemRevisionLocaleProperties == null) {
+            itemRevisionLocaleProperties = new ItemRevisionLocaleProperties();
+        }
+
+        itemRevisionLocaleProperties.setName("testItemRevision_" + RandomFactory.getRandomStringOfAlphabet(10));
+        itemRevisionLocaleProperties.setCommunityForumLink("http://www.gooooooogle.com");
+        itemRevisionLocaleProperties.setManualDocument("http://www.gooooooogle.com");
+        itemRevisionLocaleProperties.setSupportEmail("aaa@wansan.com");
+
+        locales.put(defaultLocale, itemRevisionLocaleProperties);
+
+        itemRevision.setLocales(locales);
+        itemRevision = itemRevisionService.updateItemRevision(itemRevision.getRevisionId(), itemRevision);
+
+        //update sku
+        itemRevision.setSku(RandomFactory.getRandomStringOfAlphabet(20));
+        itemRevision = itemRevisionService.updateItemRevision(itemRevision.getRevisionId(), itemRevision);
+
+        //update distributionChannels
+        List<String> channels = new ArrayList<>();
+        channels.add("Store");
+        itemRevision.setDistributionChannels(channels);
+        itemRevisionService.updateItemRevision(itemRevision.getRevisionId(), itemRevision);
+
+    }
+
+    @Property(
+            priority = Priority.Dailies,
+            features = "Put v1/item-revisions/{itemRevisionId}",
+            component = Component.Catalog,
+            owner = "JasonFu",
+            status = Status.Enable,
+            description = "Test put item revision successfully",
+            steps = {
+                    "1. Prepare a default item revision",
+                    "2. test invalid values(like null, not null and some invalid enum values)",
+                    "3. Verify the expected error"
+            }
+    )
+    @Test
+    public void testPutItemRevisionInvalidScenarios() throws Exception {
+        ItemRevision itemRevision = itemRevisionService.postDefaultItemRevision(item1);
+        ItemRevision itemRevisionTmp = itemRevisionService.postDefaultItemRevision(item1);
+
+        String itemRevisionId = itemRevision.getRevisionId();
+        String itemRevisionRev = itemRevision.getRev();
+
+        //not existed id
+        String invalidRevisionId = "AAAA";
+        verifyExpectedFailure(invalidRevisionId, itemRevision, 404);
+
+        //existed but not its revision id
+        verifyExpectedFailure(itemRevisionTmp.getRevisionId(), itemRevision);
+
+        //update revision id
+        itemRevision.setRevisionId(invalidRevisionId);
+        verifyExpectedFailure(itemRevisionId, itemRevision);
+
+        itemRevision.setRevisionId(itemRevisionTmp.getRevisionId());
+        verifyExpectedFailure(itemRevisionId, itemRevision);
+
+        itemRevision.setRevisionId(itemRevisionId);
+
+        //update rev
+        itemRevision.setRev(RandomFactory.getRandomStringOfAlphabet(10));
+        verifyExpectedFailure(itemRevisionId, itemRevision);
+
+        itemRevision.setRev(itemRevisionRev);
+
+        //set status to null or invalid string(not enum value)
+        itemRevision.setStatus(null);
+        verifyExpectedFailure(itemRevisionId, itemRevision);
+
+        itemRevision.setStatus("invalidEnumValue");
+        verifyExpectedFailure(itemRevisionId, itemRevision);
+
+        itemRevision.setStatus(CatalogEntityStatus.DRAFT.name());
+
+        //set publisher to null
+        itemRevision.setOwnerId(null);
+        verifyExpectedFailure(itemRevisionId, itemRevision);
+
+        itemRevision.setOwnerId(organizationId);
+
+        //ItemId to null or not existed value or to another item
+        itemRevision.setItemId(null);
+        verifyExpectedFailure(itemRevisionId, itemRevision);
+
+        itemRevision.setItemId(invalidRevisionId);
+        verifyExpectedFailure(itemRevisionId, itemRevision, 404);
+
+        itemRevision.setItemId(item2.getItemId());
+        if (item2.getType().equalsIgnoreCase(CatalogItemType.APP.name()) ||
+                item2.getType().equalsIgnoreCase(CatalogItemType.DOWNLOADED_ADDITION.name())) {
+            Map<String, Binary> binaries = new HashMap<>();
+            Binary binary = new Binary();
+            binary.setVersion("1");
+            binary.setSize(1024L);
+            binary.setMd5("abcdabcdabcdabcdabcdabcdabcdabcd");
+            binary.setHref("http://www.google.com/downlaod/angrybird1_0.exe");
+            binaries.put("PC", binary);
+            itemRevision.setBinaries(binaries);
+            itemRevision.setDownloadName(RandomFactory.getRandomStringOfAlphabet(10));
+        } else {
+            itemRevision.setBinaries(null);
+            itemRevision.setDownloadName(null);
+        }
+
+        itemRevision = itemRevisionService.updateItemRevision(itemRevisionId, itemRevision);
+
+        //check futureExpansion
+        Map<String, JsonNode> futureExpansion = new HashMap<>();
+
+        String str = "{\"subCountry\":\"TX\",\"street1\":\"800 West Campbell Road\"," +
+                "\"city\":\"Richardson\",\"postalCode\":\"75080\"," +
+                "\"country\":{\"id\":\"US\"}}";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode value = mapper.readTree(str);
+
+        futureExpansion.put("Address", value);
+        itemRevision.setFutureExpansion(futureExpansion);
+
+        itemRevision.setStatus(CatalogEntityStatus.DRAFT.name());
+        verifyExpectedFailure(itemRevisionId, itemRevision);
+
     }
 
     @Property(
@@ -135,4 +320,19 @@ public class TestPutItemRevision extends BaseTestClass {
             logger.logInfo("Expected exception");
         }
     }
+
+    private void verifyExpectedFailure(String itemRevisionId, ItemRevision itemRevision) throws Exception {
+        verifyExpectedFailure(itemRevisionId, itemRevision, 400);
+    }
+
+    private void verifyExpectedFailure(String itemRevisionId, ItemRevision itemRevision, int expectedResponseCode) throws Exception {
+        try {
+            itemRevisionService.updateItemRevision(itemRevisionId, itemRevision, expectedResponseCode);
+            Assert.fail("should return expected exception");
+        }
+        catch (Exception ex) {
+            logger.logInfo("Expected exception: " + ex);
+        }
+    }
+
 }
