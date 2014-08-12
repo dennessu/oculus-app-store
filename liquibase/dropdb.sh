72ef8d6e80417823fde0b5a92519646641c503b9
txchen <txchen@silkcloud.com>
2014-08-12 09:47:15 -0700

    make liquibase script runnable from any folder
    Summary: make the scripts runnable from any folder, so that automation get easier
    
    Test Plan: manual
    
    Reviewers: shuz
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D875

--- liquibase/dropdb.sh
+++ liquibase/dropdb.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 (set -o igncr) 2>/dev/null && set -o igncr; # ignore \r in windows. The comment is needed.
 set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [[ "$#" -eq 0 ]]; then
     echo "usage: $0 -env:ENVIRONMENT"
-    echo "example: $0 -env:int1box"
+    echo "example: $0 -env:onebox.int"
     exit 1
 fi
 
 dbVersion=0
 
+cd $DIR
 for dbname in `ls -d changelogs/*/$dbVersion | cut -f2 -d'/'`
 do
     if [[ ! -f "changelogs/$dbname/disabled.txt" ]]; then
         python ./dbcmd.py -db:$dbname -ver:$dbVersion -cmd:drop -yes "$@" &
     fi
 done
+cd -
 
 FAILED=0
 for job in `jobs -p`; do
