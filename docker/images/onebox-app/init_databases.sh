a52f39df991d907d440934e57cb0ac7f0ea7d4e2
txchen <txchen@silkcloud.com>
2014-08-11 23:12:46 -0700

    update onebox-app docker image

--- docker/images/onebox-app/init_databases.sh
+++ docker/images/onebox-app/init_databases.sh
@@ -9,11 +9,31 @@ function error_exit {
 shopt -s expand_aliases
 alias die='error_exit "Error ${0}(@`echo $(( $LINENO - 1 ))`):"'
 
+# start rinetd if it is not running, this is useful in on-demand docker run
+if ! pgrep rinetd; then
+  /etc/service/rinetd/run&
+  sleep 2
+fi
 # check if the database ports can be reached
 # this cannot be put into checkdockerenv.sh because that script would be executed before rinetd.sh
 nc -z 127.0.0.1 11211 || die "cannot reach memcached via port 11211, cannot continue"
 nc -z 127.0.0.1 5432 || die "cannot reach psql via port 5432, cannot continue"
 nc -z 127.0.0.1 5984 || die "cannot reach couchdb via port 5984, cannot continue"
 
-echo "populate db....."
-# TODO
+source /etc/my_init.d/checkdockerenv.sh
+echo "preparing databases....."
+SC_APP_DIR=/var/silkcloud/apphost
+
+python $SC_APP_DIR/dbsetup/memcache/flush-all.py
+echo "memcached cleared."
+
+$SC_APP_DIR/dbsetup/liquibase/createdb.sh -env:$SC_ENVIRONMENT -key:D58BA755FF96B35A6DABA7298F7A8CE2
+$SC_APP_DIR/dbsetup/liquibase/updatedb.sh -env:$SC_ENVIRONMENT -key:D58BA755FF96B35A6DABA7298F7A8CE2
+echo "psql dbs created."
+
+python $SC_APP_DIR/dbsetup/couchdb/couchdbcmd.py createdbs $SC_ENVIRONMENT --prefix=$SC_CLOUDANT_PREFIX --yes
+echo "couchdb/cloudant dbs created."
+
+# TODO: load masterkey and dataloader
+
+echo "finished preparing databases."
