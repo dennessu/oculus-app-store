9568ffe9934dc7f63bf9aa3940d703f3e5e7db03
xmchen <xmchen@wan-san.com>
2014-08-14 16:19:16 +0800

    add address validation

---
+++ billing/billing-spec/src/main/groovy/com/junbo/billing/spec/resource/AddressValidatorResource.java
@@ -0,0 +1,13 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (C) 2014 Junbo and/or its affiliates. All rights reserved.
+ */
+
+package com.junbo.billing.spec.resource;
+
+/**
+ * Created by xmchen on 14-8-14.
+ */
+public class AddressValidatorResource {
+}
