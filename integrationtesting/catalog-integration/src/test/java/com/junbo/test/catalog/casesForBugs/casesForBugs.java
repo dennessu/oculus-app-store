21bcbbc0614c3ffffe164d750635250fae5e3cd1
JasonFu <jasonfu@silkcloud.com>
2014-08-14 18:47:12 +0800

    fix failed test case due to dev change
    Summary: fix failed test case due to dev change
    
    Test Plan: build
    
    Reviewers: jiefeng
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D926

--- integrationtesting/catalog-integration/src/test/java/com/junbo/test/catalog/casesForBugs/casesForBugs.java
+++ integrationtesting/catalog-integration/src/test/java/com/junbo/test/catalog/casesForBugs/casesForBugs.java
@@ -234,6 +234,7 @@ public class casesForBugs extends BaseTestClass {
     )
     @Test
     public void testSameOwnerId() throws Exception {
+        prepareCatalogAdminToken();
 
         //item and item revision
         Organization organization1 = organizationService.postDefaultOrganization();
@@ -294,11 +295,9 @@ public class casesForBugs extends BaseTestClass {
         offerRevision.setItems(itemEntities);
         offerRevision.setOwnerId(organizationId2);
         Master.getInstance().setCurrentUid(IdConverter.idToHexString(organization2.getOwnerId()));
-        try {
-            offerRevisionService.updateOfferRevision(offerRevision.getRevisionId(), offerRevision, 400);
-        } catch (Exception ex) {
-            logger.logInfo("Expected exception");
-        }
+
+        //Due to bug 460, Item and offer could have different ownerId
+        offerRevision = offerRevisionService.updateOfferRevision(offerRevision.getRevisionId(), offerRevision);
 
         itemEntry.setItemId(item.getItemId());
         itemEntities.clear();
