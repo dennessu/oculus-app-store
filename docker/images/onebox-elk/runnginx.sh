9f489cbaf480e97fe51497931ff85b6bab4943d7
txchen <txchen@silkcloud.com>
2014-08-29 17:46:02 -0700

    fix onebox-elk bug

--- docker/images/onebox-elk/runnginx.sh
+++ docker/images/onebox-elk/runnginx.sh
@@ -5,7 +5,4 @@ set -e
 sed -e "s/^[ \t]*elasticsearch:.*/elasticsearch: \"http:\/\/\"+window.location.hostname+\":$KIBANA_PORT\",/" -i \
   /var/elk/kibana/config.js
 
-# change kibana_site.conf, fill in the port
-sed -e "s/listen[ \t]*[*][:][[:digit:]]*;/listen *:$KIBANA_PORT;/" -i /etc/nginx/sites-enabled/kibana_site.conf
-
 /usr/sbin/nginx -c /etc/nginx/nginx.conf
