2e3cbff464bf00f3746d0de4910376481a531e35
yunlongzhao <yunlongzhao@wan-san.com>
2014-09-03 17:39:37 +0800

    update store testing
    Summary: update store testing
    
    Test Plan: no
    
    Reviewers: jiefeng, jasonfu
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D1166

--- integrationtesting/store-integration/src/test/java/com/junbo/test/store/StoreCommerceTesting.java
+++ integrationtesting/store-integration/src/test/java/com/junbo/test/store/StoreCommerceTesting.java
@@ -1,25 +1,18 @@
 package com.junbo.test.store;
 
-import com.junbo.catalog.spec.model.offer.Action;
-import com.junbo.catalog.spec.model.offer.Offer;
-import com.junbo.catalog.spec.model.offer.OfferRevision;
 import com.junbo.common.id.PaymentInstrumentId;
 import com.junbo.store.spec.model.billing.BillingProfileGetResponse;
-import com.junbo.store.spec.model.billing.Instrument;
 import com.junbo.store.spec.model.billing.InstrumentUpdateResponse;
 import com.junbo.store.spec.model.login.AuthTokenResponse;
 import com.junbo.store.spec.model.login.CreateUserRequest;
 import com.junbo.store.spec.model.purchase.CommitPurchaseResponse;
 import com.junbo.store.spec.model.purchase.PreparePurchaseResponse;
-import com.junbo.test.common.blueprint.Master;
 import com.junbo.test.common.libs.IdConverter;
 import com.junbo.test.common.property.Component;
 import com.junbo.test.common.property.Priority;
 import com.junbo.test.common.property.Property;
 import com.junbo.test.common.property.Status;
 import org.testng.annotations.Test;
-
-import java.util.List;
 
 /**
  * Created by weiyu_000 on 8/29/14.
@@ -82,7 +75,7 @@ public class StoreCommerceTesting extends BaseTestClass {
     }
 
     @Property(
-            priority = Priority.BVT,
+            priority = Priority.Dailies,
             features = "Store commerce",
             component = Component.STORE,
             owner = "ZhaoYunlong",
@@ -143,9 +136,6 @@ public class StoreCommerceTesting extends BaseTestClass {
 
         PaymentInstrumentId paymentId = response.getBillingProfile().getInstruments().get(0).getSelf();
 
-        testDataProvider.preparePurchase(null, offerId, null, null, null, false, 412);
-        testDataProvider.CreateStoredValue();
-
         PreparePurchaseResponse preparePurchaseResponse = testDataProvider.preparePurchase(null, offerId, null, null, null);
 
         preparePurchaseResponse = testDataProvider.preparePurchase(preparePurchaseResponse.getPurchaseToken(),
@@ -157,29 +147,299 @@ public class StoreCommerceTesting extends BaseTestClass {
         String purchaseToken = preparePurchaseResponse.getPurchaseToken(); //get order id
 
         CommitPurchaseResponse commitPurchaseResponse = testDataProvider.commitPurchase(uid, purchaseToken);
+        validationHelper.verifyCommitPurchase(commitPurchaseResponse, offerId);
 
-        response = testDataProvider.getBillingProfile(null);
-        assert response.getBillingProfile().getInstruments().size() == 2;
-        Instrument instrument = null;
-        for (int index = 0; index < response.getBillingProfile().getInstruments().size(); index ++) {
-            if (response.getBillingProfile().getInstruments().get(index).getType().equalsIgnoreCase("STOREDVALUE")) {
-                instrument = response.getBillingProfile().getInstruments().get(index);
-            }
-        }
-        assert instrument != null;
-
-        Offer offer = Master.getInstance().getOffer(offerId);
-        OfferRevision offerRevision = Master.getInstance().getOfferRevision(offer.getCurrentRevisionId());
-        List<Action> actionList = offerRevision.getEventActions().get("PURCHASE");
-        Action action = null;
-        for(int index = 0; index < actionList.size(); index ++) {
-            if (actionList.get(index).getType().equalsIgnoreCase("CREDIT_WALLET")) {
-                action = actionList.get(index);
-            }
-        }
-        assert action != null;
-
-        assert instrument.getStoredValueBalance().equals(action.getStoredValueAmount());
-        assert instrument.getStoredValueCurrency().equalsIgnoreCase(action.getStoredValueCurrency());
+        response = testDataProvider.getBillingProfile(offerId);
+        assert response.getBillingProfile().getInstruments().size() == 1;
+        assert response.getBillingProfile().getInstruments().get(0).getType().equals("STOREDVALUE");
     }
+
+    @Property(
+            priority = Priority.Dailies,
+            features = "Store commerce",
+            component = Component.STORE,
+            owner = "ZhaoYunlong",
+            status = Status.Disable,
+            description = "Test free purchase with invalid offer id",
+            steps = {
+                    "1. Create user",
+                    "2. Make free purchase with invalid offer id",
+                    "8. Verify error response"
+            }
+    )
+    @Test
+    public void testFreePurchaseWithInvalidOfferId() throws Exception {
+
+    }
+
+
+    @Property(
+            priority = Priority.Dailies,
+            features = "Store commerce",
+            component = Component.STORE,
+            owner = "ZhaoYunlong",
+            status = Status.Disable,
+            description = "Test add instrument without billing address",
+            steps = {
+                    "1. Create user",
+                    "2. Post credit card without billing address",
+                    "8. Verify error response"
+            }
+    )
+    @Test
+    public void testFreePurchaseWithoutBillingAddress() throws Exception {
+
+    }
+
+    @Property(
+            priority = Priority.Dailies,
+            features = "Store checkout",
+            component = Component.Order,
+            owner = "ZhaoYunlong",
+            status = Status.Disable,
+            description = "Test prepare purchase with invalid offer id",
+            steps = {
+                    "1. Create user",
+                    "2. Post prepare purchase with invalid offer id",
+                    "3. Verify error response",
+            }
+    )
+    @Test
+    public void testPreparePurchaseWithInvalidOfferId() throws Exception {
+        CreateUserRequest createUserRequest = testDataProvider.CreateUserRequest();
+        AuthTokenResponse authTokenResponse = testDataProvider.CreateUser(createUserRequest, true);
+        String uid = IdConverter.idToHexString(authTokenResponse.getUserId());
+
+        PreparePurchaseResponse preparePurchaseResponse = testDataProvider.preparePurchase(null, "123", null, null, null);
+
+    }
+
+    @Property(
+            priority = Priority.Dailies,
+            features = "Store checkout",
+            component = Component.Order,
+            owner = "ZhaoYunlong",
+            status = Status.Disable,
+            description = "Test change payment via prepare purchase",
+            steps = {
+                    "1. Create user",
+                    "2. Add credit card into billing profile",
+                    "3. Add another credit card into billing profile",
+                    "3. Post prepare purchase",
+                    "5. Select the payment instrument for purchase",
+                    "6. Change the second payment via prepare purchase",
+                    "6. Commit purchase",
+                    "7. Verify purchase response",
+            }
+    )
+    @Test
+    public void testChangePaymentViaPreparePurchase() throws Exception {
+        CreateUserRequest createUserRequest = testDataProvider.CreateUserRequest();
+        AuthTokenResponse authTokenResponse = testDataProvider.CreateUser(createUserRequest, true);
+        String uid = IdConverter.idToHexString(authTokenResponse.getUserId());
+        //add new credit card to user
+
+        InstrumentUpdateResponse instrumentUpdateResponse = testDataProvider.CreateCreditCard(uid);
+        //verify decrypted credit card info
+        validationHelper.verifyAddNewCreditCard(instrumentUpdateResponse);
+
+        //get payment id in billing profile
+        PaymentInstrumentId paymentId = instrumentUpdateResponse.getBillingProfile().getInstruments().get(0).getSelf();
+
+        String offerId = testDataProvider.getOfferIdByName(offer_digital_normal1);
+        //post order without set payment instrument
+        PreparePurchaseResponse preparePurchaseResponse = testDataProvider.preparePurchase(null, offerId, null, null, null);
+
+        assert preparePurchaseResponse.getChallenge() != null;
+        assert preparePurchaseResponse.getChallenge().getType().equalsIgnoreCase("PIN");
+
+        preparePurchaseResponse = testDataProvider.preparePurchase(preparePurchaseResponse.getPurchaseToken(),
+                offerId, paymentId, "1234", null);
+
+        assert preparePurchaseResponse.getChallenge() != null;
+        assert preparePurchaseResponse.getChallenge().getType().equalsIgnoreCase("TOS_ACCEPTANCE");
+        assert preparePurchaseResponse.getChallenge().getTos() != null;
+
+        preparePurchaseResponse = testDataProvider.preparePurchase(preparePurchaseResponse.getPurchaseToken(), offerId, paymentId, null,
+                preparePurchaseResponse.getChallenge().getTos().getTosId());
+
+        //verify formatted price
+        validationHelper.verifyPreparePurchase(preparePurchaseResponse);
+
+        String purchaseToken = preparePurchaseResponse.getPurchaseToken(); //get order id
+
+        CommitPurchaseResponse commitPurchaseResponse = testDataProvider.commitPurchase(uid, purchaseToken);
+
+    }
+
+    @Property(
+            priority = Priority.Dailies,
+            features = "Store checkout",
+            component = Component.Order,
+            owner = "ZhaoYunlong",
+            status = Status.Disable,
+            description = "Test checkout with invalid payment id",
+            steps = {
+                    "1. Create user",
+                    "2. Post prepare purchase",
+                    "3. Select the invalid payment instrument for purchase",
+                    "4. Commit purchase",
+                    "5. Verify error response",
+            }
+    )
+    @Test
+    public void testCheckoutWithInvalidPaymentId() throws Exception {
+        CreateUserRequest createUserRequest = testDataProvider.CreateUserRequest();
+        AuthTokenResponse authTokenResponse = testDataProvider.CreateUser(createUserRequest, true);
+        String uid = IdConverter.idToHexString(authTokenResponse.getUserId());
+        //add new credit card to user
+
+        InstrumentUpdateResponse instrumentUpdateResponse = testDataProvider.CreateCreditCard(uid);
+        //verify decrypted credit card info
+        validationHelper.verifyAddNewCreditCard(instrumentUpdateResponse);
+
+        //get payment id in billing profile
+        PaymentInstrumentId paymentId = instrumentUpdateResponse.getBillingProfile().getInstruments().get(0).getSelf();
+
+        String offerId = testDataProvider.getOfferIdByName(offer_digital_normal1);
+        //post order without set payment instrument
+        PreparePurchaseResponse preparePurchaseResponse = testDataProvider.preparePurchase(null, offerId, null, null, null);
+
+        assert preparePurchaseResponse.getChallenge() != null;
+        assert preparePurchaseResponse.getChallenge().getType().equalsIgnoreCase("PIN");
+
+        preparePurchaseResponse = testDataProvider.preparePurchase(preparePurchaseResponse.getPurchaseToken(),
+                offerId, paymentId, "1234", null);
+
+        assert preparePurchaseResponse.getChallenge() != null;
+        assert preparePurchaseResponse.getChallenge().getType().equalsIgnoreCase("TOS_ACCEPTANCE");
+        assert preparePurchaseResponse.getChallenge().getTos() != null;
+
+        preparePurchaseResponse = testDataProvider.preparePurchase(preparePurchaseResponse.getPurchaseToken(), offerId, paymentId, null,
+                preparePurchaseResponse.getChallenge().getTos().getTosId());
+
+        //verify formatted price
+        validationHelper.verifyPreparePurchase(preparePurchaseResponse);
+
+        String purchaseToken = preparePurchaseResponse.getPurchaseToken(); //get order id
+
+        CommitPurchaseResponse commitPurchaseResponse = testDataProvider.commitPurchase(uid, purchaseToken);
+
+    }
+
+    @Property(
+            priority = Priority.Dailies,
+            features = "Store checkout",
+            component = Component.Order,
+            owner = "ZhaoYunlong",
+            status = Status.Disable,
+            description = "Test commit purchase with invalid purchase token",
+            steps = {
+                    "1. Create user",
+                    "2. Add credit card into billing profile",
+                    "3. Post prepare purchase",
+                    "4. Select payment instrument for purchase",
+                    "5. Commit purchase with invalid purchase token",
+                    "7. Verify error response",
+            }
+    )
+    @Test
+    public void testCommitPurchaseWithInvalidPurchaseToken() throws Exception {
+        CreateUserRequest createUserRequest = testDataProvider.CreateUserRequest();
+        AuthTokenResponse authTokenResponse = testDataProvider.CreateUser(createUserRequest, true);
+        String uid = IdConverter.idToHexString(authTokenResponse.getUserId());
+        //add new credit card to user
+
+        InstrumentUpdateResponse instrumentUpdateResponse = testDataProvider.CreateCreditCard(uid);
+        //verify decrypted credit card info
+        validationHelper.verifyAddNewCreditCard(instrumentUpdateResponse);
+
+        //get payment id in billing profile
+        PaymentInstrumentId paymentId = instrumentUpdateResponse.getBillingProfile().getInstruments().get(0).getSelf();
+
+        String offerId = testDataProvider.getOfferIdByName(offer_digital_normal1);
+        //post order without set payment instrument
+        PreparePurchaseResponse preparePurchaseResponse = testDataProvider.preparePurchase(null, offerId, null, null, null);
+
+        assert preparePurchaseResponse.getChallenge() != null;
+        assert preparePurchaseResponse.getChallenge().getType().equalsIgnoreCase("PIN");
+
+        preparePurchaseResponse = testDataProvider.preparePurchase(preparePurchaseResponse.getPurchaseToken(),
+                offerId, paymentId, "1234", null);
+
+        assert preparePurchaseResponse.getChallenge() != null;
+        assert preparePurchaseResponse.getChallenge().getType().equalsIgnoreCase("TOS_ACCEPTANCE");
+        assert preparePurchaseResponse.getChallenge().getTos() != null;
+
+        preparePurchaseResponse = testDataProvider.preparePurchase(preparePurchaseResponse.getPurchaseToken(), offerId, paymentId, null,
+                preparePurchaseResponse.getChallenge().getTos().getTosId());
+
+        //verify formatted price
+        validationHelper.verifyPreparePurchase(preparePurchaseResponse);
+
+        String purchaseToken = preparePurchaseResponse.getPurchaseToken(); //get order id
+
+        CommitPurchaseResponse commitPurchaseResponse = testDataProvider.commitPurchase(uid, purchaseToken);
+
+    }
+
+
+    @Property(
+            priority = Priority.Dailies,
+            features = "Store checkout",
+            component = Component.Order,
+            owner = "ZhaoYunlong",
+            status = Status.Disable,
+            description = "Test commit purchase with another purchase token",
+            steps = {
+                    "1. Create user",
+                    "2. Add credit card into billing profile",
+                    "3. Post prepare purchase",
+                    "4. Select payment instrument for purchase",
+                    "5. Do the previous steps again to get another purchase token",
+                    "6. Commit purchase with another purchase token ",
+                    "7. Verify error response",
+            }
+    )
+    @Test
+    public void testCommitPurchaseWithAnotherPurchaseToken() throws Exception {
+        CreateUserRequest createUserRequest = testDataProvider.CreateUserRequest();
+        AuthTokenResponse authTokenResponse = testDataProvider.CreateUser(createUserRequest, true);
+        String uid = IdConverter.idToHexString(authTokenResponse.getUserId());
+        //add new credit card to user
+
+        InstrumentUpdateResponse instrumentUpdateResponse = testDataProvider.CreateCreditCard(uid);
+        //verify decrypted credit card info
+        validationHelper.verifyAddNewCreditCard(instrumentUpdateResponse);
+
+        //get payment id in billing profile
+        PaymentInstrumentId paymentId = instrumentUpdateResponse.getBillingProfile().getInstruments().get(0).getSelf();
+
+        String offerId = testDataProvider.getOfferIdByName(offer_digital_normal1);
+        //post order without set payment instrument
+        PreparePurchaseResponse preparePurchaseResponse = testDataProvider.preparePurchase(null, offerId, null, null, null);
+
+        assert preparePurchaseResponse.getChallenge() != null;
+        assert preparePurchaseResponse.getChallenge().getType().equalsIgnoreCase("PIN");
+
+        preparePurchaseResponse = testDataProvider.preparePurchase(preparePurchaseResponse.getPurchaseToken(),
+                offerId, paymentId, "1234", null);
+
+        assert preparePurchaseResponse.getChallenge() != null;
+        assert preparePurchaseResponse.getChallenge().getType().equalsIgnoreCase("TOS_ACCEPTANCE");
+        assert preparePurchaseResponse.getChallenge().getTos() != null;
+
+        preparePurchaseResponse = testDataProvider.preparePurchase(preparePurchaseResponse.getPurchaseToken(), offerId, paymentId, null,
+                preparePurchaseResponse.getChallenge().getTos().getTosId());
+
+        //verify formatted price
+        validationHelper.verifyPreparePurchase(preparePurchaseResponse);
+
+        String purchaseToken = preparePurchaseResponse.getPurchaseToken(); //get order id
+
+        CommitPurchaseResponse commitPurchaseResponse = testDataProvider.commitPurchase(uid, purchaseToken);
+
+    }
+
+
 }
