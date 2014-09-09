30a606d939743996f9dd8fea4936d4938cac2a05
fzhang <fzhang@silkcloud.com>
2014-09-09 14:53:30 +0800

    change image/category/genres

--- store/store-clientproxy/src/main/groovy/com/junbo/store/clientproxy/catalog/CatalogFacade.java
+++ store/store-clientproxy/src/main/groovy/com/junbo/store/clientproxy/catalog/CatalogFacade.java
@@ -8,7 +8,6 @@ package com.junbo.store.clientproxy.catalog;
 import com.junbo.common.enumid.LocaleId;
 import com.junbo.common.model.Results;
 import com.junbo.langur.core.promise.Promise;
-import com.junbo.store.spec.model.catalog.Category;
 import com.junbo.store.spec.model.catalog.Item;
 import com.junbo.store.spec.model.catalog.Offer;
 import com.junbo.store.spec.model.iap.IAPOfferGetRequest;
@@ -27,10 +26,6 @@ public interface CatalogFacade {
     Promise<Results<Item>> getItemsByCategory(String categoryId, Long cursor, Long count);
 
     Promise<Item> getItemByPackageName(String packageName);
-
-    Promise<Category> getCategory(String categoryId);
-
-    Promise<List<Category>> getCategoriesByParent(String parentId);
 
     Promise<List<Offer>> getInAppOffers(Item hostItem, IAPOfferGetRequest request);
 
