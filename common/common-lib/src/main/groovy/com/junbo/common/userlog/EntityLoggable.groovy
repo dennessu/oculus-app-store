84bdd08c7cbf1325d13ee59556a5b89ef3c0d160
NanXin <nanxin@silkcloud.com>
2014-09-12 15:28:01 +0800

    add switch of user log and add entity log interface
    Summary: add switch of user log and add entity log interface
    
    Test Plan: browser
    
    Reviewers: fzhang, shuz
    
    Reviewed By: shuz
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D1229

---
+++ common/common-lib/src/main/groovy/com/junbo/common/userlog/EntityLoggable.groovy
@@ -0,0 +1,13 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (C) 2014 Junbo and/or its affiliates. All rights reserved.
+ */
+package com.junbo.common.userlog
+
+/**
+ * Created by x on 9/12/14.
+ */
+public interface EntityLoggable {
+    String getEntityLogId()
+}
