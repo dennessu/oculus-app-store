74bb4bacdc69f486362f4b3ccfd64c26acc552ae
shuz <shuz@silkcloud.com>
2014-09-16 17:55:12 +0800

    Add PROD environment blue.
    Summary: Add PROD environment blue.
    
    Test Plan: manual
    
    Reviewers: yilin, nanxin, kg, txchen
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D1260

---
+++ scripts/automation/upgrade-prod.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+pushd $DIR
+
+set -e
+
+. ./readargs.sh
+
+# optional: ./reset-config.sh
+# in case of new db servers: ./setup-dbauth.sh
+
+./setup-couch.sh
+echo "Please verify cloudant is properly updated."
+read
+./upgrade-dbs.sh
+echo "Please verify db upgrade is properly updated."
+read
+./upgrade-utils.sh
+echo "Please verify new utils are working properly."
+read
+./upgrade-apps.sh
+echo "Please verify new apps are taking traffic. Press enter to continue."
+read
+./dataloader.sh
+
+./check-versions.sh
+
