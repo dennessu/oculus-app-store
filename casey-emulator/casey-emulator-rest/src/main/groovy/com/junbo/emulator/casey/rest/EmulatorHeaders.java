0e43d2e80e588ea065d2fcdcf4a6054f784a085f
fzhang <fzhang@silkcloud.com>
2014-09-23 12:21:51 +0800

    fix casey emulator

--- casey-emulator/casey-emulator-rest/src/main/groovy/com/junbo/emulator/casey/rest/EmulatorHeaders.java
+++ casey-emulator/casey-emulator-rest/src/main/groovy/com/junbo/emulator/casey/rest/EmulatorHeaders.java
@@ -9,6 +9,6 @@ package com.junbo.emulator.casey.rest;
  * The EmulatorHeaders class.
  */
 public enum  EmulatorHeaders {
-    X_EMULATE_LATENCY,
-    X_REVIEW_USER_ID
+    X_QA_CASEY_EMULATE_LATENCY,
+    X_QA_CASEY_REVIEW_USER_ID_LIST,
 }
