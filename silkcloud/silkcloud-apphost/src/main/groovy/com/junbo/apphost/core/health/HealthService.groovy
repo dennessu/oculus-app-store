02e07203845c1039f642aa00d44e2933ab7584a2
shuz <shuz@silkcloud.com>
2014-11-21 16:06:49 +0800

    Fix health check on cyrpto server.
    Summary: Fix health check on cyrpto server.
    
    Test Plan: supercycle
    
    Reviewers: yilin
    
    Reviewed By: yilin
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D1587

--- silkcloud/silkcloud-apphost/src/main/groovy/com/junbo/apphost/core/health/HealthService.groovy
+++ silkcloud/silkcloud-apphost/src/main/groovy/com/junbo/apphost/core/health/HealthService.groovy
@@ -11,6 +11,7 @@ import groovy.transform.CompileStatic
 import org.apache.commons.lang3.exception.ExceptionUtils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Required
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.TransactionDefinition
@@ -35,7 +36,7 @@ class HealthService {
     private CryptoResource cryptoResource;
     private PlatformTransactionManager transactionManager
 
-    @Required
+    @Autowired(required = false)
     void setPingSqlRepos(List<PingSqlRepo> pingSqlRepos) {
         this.pingSqlRepos = pingSqlRepos
     }
