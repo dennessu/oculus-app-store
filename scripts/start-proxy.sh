cae53d50da27178e1b64d81143a9d8ec5b706734
shuz <shuz@silkcloud.com>
2014-11-26 14:05:47 +0800

    Change proxy setting to personal.properties
    Summary:
    Change the proxy setting to personal.properties.
    Check the IP country before trying to set the proxy.
    
    Test Plan: supercycle
    
    Reviewers: txchen, wenhongzhu
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D1606

--- scripts/start-proxy.sh
+++ scripts/start-proxy.sh
@@ -10,7 +10,7 @@ PROXY_SERVER=silkcloud@54.92.103.97
 if ! ( ssh -S .tokyo-proxy -O check $PROXY_SERVER ) 2>/dev/null; then
     echo Starting proxy...
     chmod 600 .ssh/id_rsa
-    ssh -M -S .tokyo-proxy -L 13128:127.0.0.1:3128 $PROXY_SERVER -p 9222 -fnNT -i .ssh/id_rsa
+    ssh -o "StrictHostKeyChecking no" -M -S .tokyo-proxy -L 13128:127.0.0.1:3128 $PROXY_SERVER -p 9222 -fnNT -i .ssh/id_rsa
 else
     echo Proxy already started.
 fi
