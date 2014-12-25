0efb61f0828bf3d048b5b81e1d3969e1f596426d
shuz <shuz@silkcloud.com>
2014-12-25 16:14:12 +0800

    Add Europe datacenter for staging.
    Summary: Add Europe datacenter for staging.
    
    Test Plan: manual, supercycle
    
    Reviewers: yilin, yangyang, nanxin
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D1700

---
+++ scripts/pgha/env/staging.eucentral_crypto_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#https://oculus.atlassian.net/wiki/pages/viewpage.action?spaceKey=SER&title=AWS-test+servers
+
+export MASTER_HOST=10.114.38.10
+export SLAVE_HOST=
+export REPLICA_HOST=
+export BCP_HOST=
+
+export REPLICA_DATABASES=()
+
