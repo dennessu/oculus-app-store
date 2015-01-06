704878d7c8643852829cdd901257695d3f28fa39
wenhongzhu <wenhongzhu@silkcloud.com>
2015-01-06 17:21:54 +0800

    switch to adyen as FB is failed
    Summary: switch to adyen as fb is failed
    
    Test Plan: supercycle
    
    Reviewers: fenglinyu
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D1725

--- scripts/unittests/ut_checkout.py
+++ scripts/unittests/ut_checkout.py
@@ -192,7 +192,7 @@ class CheckoutTests(ut.TestBase):
         piTypes = curlJson('GET', ut.test_uri, '/v1/payment-instrument-types')
         ccPiType = [ piType for piType in piTypes['results'] if piType['typeCode'] == 'CREDITCARD' ][0]
 
-        cardInfo = self.getFBEncryptedCardInfo()
+        cardInfo = self.getEncryptedCardInfo()
         pi = curlJson('POST', ut.test_uri, '/v1/payment-instruments', headers = {
             "Authorization": "Bearer " + user.access_token
         }, data = {
