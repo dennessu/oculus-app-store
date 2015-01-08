41d997e0ccb5563ce8ec3ca0ecb0f6cc31969569
yangyang <yangyang@silkcloud.com>
2015-01-08 06:54:41 -0800

    update configuration for asian_production
    Summary: update configuration for asian_production
    
    Test Plan: supercycle
    
    Reviewers: yilin, nanxin, shuz
    
    Reviewed By: shuz
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D1741

---
+++ scripts/pgha/env/prod.apsoutheast_0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export MASTER_HOST=10.120.16.50
+export SLAVE_HOST=10.120.20.50
+export REPLICA_HOST=10.120.22.50
+export BCP_HOST=10.20.22.52
+
+export REPLICA_DATABASES=('postgres' 'billing' 'ewallet' 'fulfilment' 'order' 'payment' 'sharding' 'subscription')
+
+# pgsql conf for powerful machines
+export MAX_CONNECTIONS=5000
+export SHARED_BUFFERS='4GB'
+export MAINTENANCE_WORK_MEM='1GB'
+export EFFECTIVE_CACHE_SIZE='10GB'
+export CHECKPOINT_SEGMENTS='64'
+export CHECKPOINT_COMPLETION_TARGET='0.8'
