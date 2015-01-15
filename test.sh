81829c78754c6817595a5cd0b86f7fbee2bc7ce1
txchen <txchen@silkcloud.com>
2015-01-14 19:05:39 -0800

    add a reduce output mode for test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,20 @@ source "$(git rev-parse --show-toplevel)/scripts/common.sh"; # this comment is n
 
 set -e
 
+OPTIND=1
+reduce_output=0
+
+# -r -> reduce_output:
+#   will not print to console if all test cases are passing
+#   if there are failures, then will print all the test log
+#   this is due to log amount is too big for web tool to display
+while getopts "r" opt; do
+  case "$opt" in
+  r) reduce_output=1
+    ;;
+  esac
+done
+
 t0=`date +%s`
 
 cd `git rev-parse --show-toplevel`
@@ -21,6 +35,10 @@ function cleanupTrap {
     echo !!!!!!!!!!!!!!!!
     echo Test has ERRORs!
     echo !!!!!!!!!!!!!!!!
+    if [[ "$reduce_output" = "1" ]]; then
+      echo "Print the supercycle_suite.log:"
+      cat logs/supercycle_suite.log
+    fi
 }
 
 trap cleanupTrap EXIT INT TERM
@@ -30,9 +48,14 @@ trap cleanupTrap EXIT INT TERM
 # make sure the error code of python will be returned
 set -o pipefail
 
+echo "apphost started, start running test cases"
 cd ./scripts/unittests/
 mkdir -p logs
-python ./supercycle_suite.py 2>&1 | tee logs/supercycle_suite.log
+if [[ "$reduce_output" = "1" ]]; then
+  python ./supercycle_suite.py 2>&1 >logs/supercycle_suite.log
+else
+  python ./supercycle_suite.py 2>&1 | tee logs/supercycle_suite.log
+fi
 
 trap - EXIT INT TERM
 cleanup
