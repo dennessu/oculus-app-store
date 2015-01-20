20e84b12ffee4965010114adde2aa93e45ca06a6
wenhongzhu <wenhongzhu@silkcloud.com>
2015-01-20 18:21:25 +0800

    enable fb bvt
    Summary: enable fb bvt
    
    Test Plan: supercycle
    
    Reviewers: shuz
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D1779

--- scripts/unittests/ut_checkout.py
+++ scripts/unittests/ut_checkout.py
@@ -129,6 +129,7 @@ class CheckoutTests(ut.TestBase):
         }
 
     def testCheckout(self):
+        cardInfo = self.getFBEncryptedCardInfo()
         user = oauth.testRegister('identity commerce')
         devinfo = self.testDeveloper()
 
@@ -195,7 +196,7 @@ class CheckoutTests(ut.TestBase):
         piTypes = curlJson('GET', ut.test_uri, '/v1/payment-instrument-types')
         ccPiType = [ piType for piType in piTypes['results'] if piType['typeCode'] == 'CREDITCARD' ][0]
 
-        cardInfo = self.getEncryptedCardInfo()
+        cardInfo = self.getFBEncryptedCardInfo()
         pi = curlJson('POST', ut.test_uri, '/v1/payment-instruments', headers = {
             "Authorization": "Bearer " + user.access_token
         }, data = {
