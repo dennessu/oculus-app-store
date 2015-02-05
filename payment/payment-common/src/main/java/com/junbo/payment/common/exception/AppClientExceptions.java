75b07bddd5ce80819f67acd036613a3e88b74935
wenhongzhu <wenhongzhu@silkcloud.com>
2015-02-05 11:32:01 +0800

    update error code
    Summary: update error code
    
    Test Plan: supercycle
    
    Reviewers: fenglinyu
    
    Subscribers: all
    
    Differential Revision: https://code.silkcloud.info/D1841

--- payment/payment-common/src/main/java/com/junbo/payment/common/exception/AppClientExceptions.java
+++ payment/payment-common/src/main/java/com/junbo/payment/common/exception/AppClientExceptions.java
@@ -18,66 +18,66 @@ import com.junbo.common.error.ErrorProxy;
 public interface AppClientExceptions {
     AppClientExceptions INSTANCE = ErrorProxy.newProxyInstance(AppClientExceptions.class);
 
-    @ErrorDef(httpStatusCode = 412, code = "401", message = "User Not Found.", reason = "The user_id {0} is invalid", field = "user_id")
+    @ErrorDef(httpStatusCode = 412, code = "101", message = "User Not Found.", reason = "The user_id {0} is invalid", field = "user_id")
     AppError userNotFound(String userId);
 
-    @ErrorDef(httpStatusCode = 412, code = "402", message = "User Name Not Found.", reason = "The user name {0} is invalid")
+    @ErrorDef(httpStatusCode = 412, code = "102", message = "User Name Not Found.", reason = "The user name {0} is invalid")
     AppError userNameNotFound(String userPiiId);
 
-    @ErrorDef(httpStatusCode = 412, code = "403", message = "Invalid Payment Instrument Id.",
+    @ErrorDef(httpStatusCode = 412, code = "103", message = "Invalid Payment Instrument Id.",
         reason = "The payment_instrument_id {0} is invalid", field = "payment_instrument_id")
     AppError invalidPaymentInstrumentId(String piId);
 
-    @ErrorDef(httpStatusCode = 412, code = "404", message = "Invalid Amount.",
+    @ErrorDef(httpStatusCode = 412, code = "104", message = "Invalid Amount.",
             reason = "The amount {0} is invalid or not allowed", field = "amount")
     AppError invalidAmount(String amount);
 
-    @ErrorDef(httpStatusCode = 412, code = "405", message = "Currency Not Found.",
+    @ErrorDef(httpStatusCode = 412, code = "105", message = "Currency Not Found.",
             reason = "The currency {0} is invalid or not allowed", field = "currency")
     AppError currencyNotFound(String currency);
 
-    @ErrorDef(httpStatusCode = 412, code = "406", message = "Country Not Found.",
+    @ErrorDef(httpStatusCode = 412, code = "106", message = "Country Not Found.",
             reason = "The country {0} is invalid or not allowed", field = "country")
     AppError countryNotFound(String country);
 
-    @ErrorDef(httpStatusCode = 412, code = "407", message = "Invalid PI Type.",
+    @ErrorDef(httpStatusCode = 412, code = "107", message = "Invalid PI Type.",
             reason = "The payment instrument type {0} is invalid or not allowed"
             , field = "payment_instrument_type")
     AppError invalidPIType(String piType);
 
-    @ErrorDef(httpStatusCode = 404, code = "408", message = "Payment Instrument Not Found.",
+    @ErrorDef(httpStatusCode = 404, code = "108", message = "Payment Instrument Not Found.",
             reason = "The payment id {0} is not found", field = "payment_id")
     AppError paymentInstrumentNotFound(String paymentId);
 
-    @ErrorDef(httpStatusCode = 412, code = "409", message = "Insufficient Balance.",
+    @ErrorDef(httpStatusCode = 412, code = "109", message = "Insufficient Balance.",
             reason = "The Stored Value Balance is insufficient")
     AppError insufficientBalance();
 
-    @ErrorDef(httpStatusCode = 412, code = "410", message = "Billing Address Not Found.",
+    @ErrorDef(httpStatusCode = 412, code = "110", message = "Billing Address Not Found.",
             reason = "The billing address id {0} is invalid")
     AppError billingAddressNotFound(String billingAddressId);
 
-    @ErrorDef(httpStatusCode = 412, code = "411", message = "Invalid Billing Address Id",
+    @ErrorDef(httpStatusCode = 412, code = "111", message = "Invalid Billing Address Id",
             reason = "The billing address id {0} is invalid due to {1}")
     AppError invalidBillingAddressId(String billingAddressId, String reason);
 
-    @ErrorDef(httpStatusCode = 412, code = "412", message = "Missing Email",
+    @ErrorDef(httpStatusCode = 412, code = "112", message = "Missing Email",
             reason = "the email info is missing")
     AppError missingEmail();
 
-    @ErrorDef(httpStatusCode = 412, code = "413", message = "invalid BIlling Ref",
+    @ErrorDef(httpStatusCode = 412, code = "113", message = "invalid BIlling Ref",
             reason = "the billing reference id is invalid: {0}")
     AppError invalidBIllingRef(String billingRef);
 
-    @ErrorDef(httpStatusCode = 412, code = "414", message = "invalid field to update",
+    @ErrorDef(httpStatusCode = 412, code = "114", message = "invalid field to update",
             reason = "{0} is not allow to be updated")
     AppError updateNotAllowed(String fieldName);
 
-    @ErrorDef(httpStatusCode = 412, code = "415", message = "invalid user to call the api",
+    @ErrorDef(httpStatusCode = 412, code = "115", message = "invalid user to call the api",
             reason = "{0} is not allowed")
     AppError userNotAllowed(String userId);
 
-    @ErrorDef(httpStatusCode = 400, code = "416", message = "invalid request for provider",
+    @ErrorDef(httpStatusCode = 400, code = "116", message = "invalid request for provider",
             reason = "provider {0} with error details: {1}")
     AppError providerInvalidRequest(String providerName, String error);
 
